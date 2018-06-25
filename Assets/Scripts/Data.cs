7a253b0277d5e4525e57525906f5888d47b45a3e
高橋碧 <>
2018-06-26 04:12:11 +0900

    Add namespace Data

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,11 @@
+﻿// これらの定数を使用するファイルの上部に "using Data;" と記述し、WindowSize.WIDTHなどで参照することを現在は推奨
+
+namespace Data {
+	public static class WindowSize {
+		public const int WIDTH = 900;
+		public const int HEIGHT = 1600;
+
+		public const int BRANK_WIDTH = 0;
+		public const int BRANK_HEIGHT = 0;
+	}
+}
