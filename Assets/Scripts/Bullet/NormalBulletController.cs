2d20413ac291f968ef4e2555f2b2cd55dee6b3f6
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2018-07-15 20:59:11 +0900

    BulletControllerがMonoBehaviourを継承するように変更

--- Assets/Scripts/Bullet/NormalBulletController.cs
+++ Assets/Scripts/Bullet/NormalBulletController.cs
@@ -1,16 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class NormalBulletController : MonoBehaviour, BulletController{
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+﻿
+public class NormalBulletController : BulletController {
+    
 }
