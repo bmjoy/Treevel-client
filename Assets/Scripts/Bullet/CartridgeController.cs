7739d75caaa97f0fe6d43104121b9bd028ab9d21
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2018-07-16 14:17:36 +0900

    Add CartridgeController.cs

---
+++ Assets/Scripts/Bullet/CartridgeController.cs
@@ -0,0 +1,13 @@
+﻿
+public class CartridgeController : BulletController {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
