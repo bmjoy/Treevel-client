5ec074e6e88491bf83a3413357a253541fffcbbb
高橋碧 <>
2018-09-05 18:07:47 +0900

    Add controller script for tiles and panels

---
+++ Assets/Scripts/Panel/PanelController.cs
@@ -0,0 +1,18 @@
+﻿using Tile;
+using UnityEngine;
+
+namespace Panel
+{
+    public class PanelController : MonoBehaviour {
+
+        // Use this for initialization
+        void Start ()
+        {
+        }
+
+        // Update is called once per frame
+        void Update ()
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/Tile/TileController.cs
@@ -0,0 +1,32 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Tile
+{
+    public class TileController : MonoBehaviour
+    {
+        [CanBeNull] private GameObject rightTile;
+        [CanBeNull] private GameObject leftTile;
+        [CanBeNull] private GameObject upperTile;
+        [CanBeNull] private GameObject lowerTile;
+
+        // Use this for initialization
+        void Start ()
+        {
+        }
+
+        // Update is called once per frame
+        void Update ()
+        {
+        }
+
+        // 自身タイルの上下左右のタイルへの参照を入れる
+        public void MakeRelation(GameObject rightTile, GameObject leftTile, GameObject upperTile, GameObject lowerTile)
+        {
+            this.rightTile = rightTile;
+            this.leftTile = leftTile;
+            this.upperTile = upperTile;
+            this.lowerTile = lowerTile;
+        }
+    }
+}
