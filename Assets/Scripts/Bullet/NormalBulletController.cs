7f36563c78395487166b20cfc38cb90eb3f53fdc
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2018-10-07 17:01:02 +0900

    set bullet position according to motionVector and localScale

--- Assets/Scripts/Bullet/NormalBulletController.cs
+++ Assets/Scripts/Bullet/NormalBulletController.cs
@@ -5,13 +5,19 @@ namespace Bullet
     public class NormalBulletController : CartridgeController
     {
         // コンストラクタがわりのメソッド
-        public override void Initialize(Vector2 motionVector)
+        public virtual void Initialize(Vector2 position, Vector2 motionVector)
         {
             base.Initialize(motionVector);
             this.speed = 0.1f;
-            width = (float) (WindowSize.WIDTH * 0.15);
-            height = width;
-            this.transform.localScale *= new Vector2(width, height);
+            this.localScale = (float) (WindowSize.WIDTH * 0.15);
+
+            if (motionVector.Equals(Vector2.right)) this.transform.position = new Vector2(-(WindowSize.WIDTH+localScale)/2, position.y);
+            else if (motionVector.Equals(Vector2.left)) this.transform.position = new Vector2((WindowSize.WIDTH+localScale)/2, position.y);
+            else if (motionVector.Equals(Vector2.up)) this.transform.position = new Vector2(position.x, -(WindowSize.HEIGHT+localScale)/2);
+            else if (motionVector.Equals(Vector2.down)) this.transform.position = new Vector2(position.x, (WindowSize.HEIGHT+localScale)/2);
+
+            // Check if a bullet should be flipped vertically
+            this.transform.localScale *= new Vector2(localScale, (-1)*Mathf.Sign(motionVector.x)*localScale);
         }
     }
 }
