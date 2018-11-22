ea2dbd6fa254623dd881bf01e5e75c2f2372969d
高橋碧 <>
2018-11-23 02:33:59 +0900

    Fix for review

--- Assets/Scripts/GamePlayScene/Bullet/CartridgeController.cs
+++ Assets/Scripts/GamePlayScene/Bullet/CartridgeController.cs
@@ -42,9 +42,22 @@ namespace GamePlayScene.Bullet
 			transform.Translate(motionVector * speed, Space.World);
 		}
 
-		// コンストラクタがわりのメソッド
-		protected void Initialize(Vector2 motionVector)
+		// 銃弾の初期配置の設定
+		protected void SetInitialPosition(Vector2 position, Vector2 motionVector)
 		{
+			if (motionVector.Equals(Vector2.right))
+				transform.position = new Vector2(-(WindowSize.WIDTH + localScale * originalWidth) / 2,
+					position.y);
+			else if (motionVector.Equals(Vector2.left))
+				transform.position = new Vector2((WindowSize.WIDTH + localScale * originalWidth) / 2,
+					position.y);
+			else if (motionVector.Equals(Vector2.up))
+				transform.position = new Vector2(position.x, -(WindowSize.HEIGHT + localScale * originalHeight) / 2);
+			else if (motionVector.Equals(Vector2.down))
+				transform.position = new Vector2(position.x, (WindowSize.HEIGHT + localScale * originalHeight) / 2);
+			// Check if a bullet should be flipped vertically
+			transform.localScale *= new Vector2(localScale, -1 * Mathf.Sign(motionVector.x) * localScale);
+			
 			this.motionVector = motionVector;
 			// Calculate rotation angle
 			var angle = Vector2.Dot(motionVector, Vector2.left) / motionVector.magnitude;
--- Assets/Scripts/GamePlayScene/Bullet/NormalCartridgeController.cs
+++ Assets/Scripts/GamePlayScene/Bullet/NormalCartridgeController.cs
@@ -7,24 +7,12 @@ namespace GamePlayScene.Bullet
 		// コンストラクタがわりのメソッド
 		public void Initialize(Vector2 position, Vector2 motionVector)
 		{
-			Initialize(motionVector);
 			speed = 0.1f;
 			originalWidth = GetComponent<Renderer>().bounds.size.x;
 			originalHeight = GetComponent<Renderer>().bounds.size.y;
 			localScale = (float) (WindowSize.WIDTH * 0.15);
 
-			if (motionVector.Equals(Vector2.right))
-				transform.position = new Vector2(-(WindowSize.WIDTH + localScale * originalWidth) / 2,
-					position.y);
-			else if (motionVector.Equals(Vector2.left))
-				transform.position = new Vector2((WindowSize.WIDTH + localScale * originalWidth) / 2,
-					position.y);
-			else if (motionVector.Equals(Vector2.up))
-				transform.position = new Vector2(position.x, -(WindowSize.HEIGHT + localScale * originalHeight) / 2);
-			else if (motionVector.Equals(Vector2.down))
-				transform.position = new Vector2(position.x, (WindowSize.HEIGHT + localScale * originalHeight) / 2);
-			// Check if a bullet should be flipped vertically
-			transform.localScale *= new Vector2(localScale, -1 * Mathf.Sign(motionVector.x) * localScale);
+			SetInitialPosition(position, motionVector);
 		}
 	}
 }
