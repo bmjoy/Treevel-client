e46b5bc12128379bf54b6809652c89ee8e6bd843
高橋碧 <>
2018-11-23 12:05:19 +0900

    Reformat code

--- Assets/Scripts/GamePlayScene/Bullet/CartridgeController.cs
+++ Assets/Scripts/GamePlayScene/Bullet/CartridgeController.cs
@@ -57,7 +57,7 @@ namespace GamePlayScene.Bullet
 				transform.position = new Vector2(position.x, (WindowSize.HEIGHT + localScale * originalHeight) / 2);
 			// Check if a bullet should be flipped vertically
 			transform.localScale *= new Vector2(localScale, -1 * Mathf.Sign(motionVector.x) * localScale);
-			
+
 			this.motionVector = motionVector;
 			// Calculate rotation angle
 			var angle = Vector2.Dot(motionVector, Vector2.left) / motionVector.magnitude;
