163e7de9b3c92bc087d735decc1a2bfd1219a0b8
高橋碧 <>
2018-12-16 03:04:37 +0900

    Create key's file for PlayerPrefs

---
+++ Assets/Project/Scripts/Library/PlayerPrefsUtils/PlayerPrefsKeys.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Project.Scripts.Library.PlayerPrefsUtils
+{
+	public class PlayerPrefsKeys : MonoBehaviour
+	{
+		public const string STAGE_STATUS_KEY = "STAGE_STATUS_KEY";
+	}
+}
