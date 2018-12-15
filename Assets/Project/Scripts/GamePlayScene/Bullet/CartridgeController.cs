d29cffa7f5a24cb113bdba6a3294f43b24773c21
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2018-12-16 05:21:05 +0900

    Reformat Code

--- Assets/Project/Scripts/GamePlayScene/Bullet/CartridgeController.cs
+++ Assets/Project/Scripts/GamePlayScene/Bullet/CartridgeController.cs
@@ -31,8 +31,9 @@ namespace Project.Scripts.GamePlayScene.Bullet
 		{
 			// BocCollider2Dのアタッチメント
 			gameObject.AddComponent<BoxCollider2D>();
-			gameObject.GetComponent<BoxCollider2D>().offset = new Vector2(-(1.5f-(WindowSize.WIDTH * 0.24f * 0.1f))/2, 0);
-			gameObject.GetComponent<BoxCollider2D>().size = new Vector2(WindowSize.WIDTH * 0.24f * 0.1f,0.5f);
+			gameObject.GetComponent<BoxCollider2D>().offset =
+				new Vector2(-(1.5f - (WindowSize.WIDTH * 0.24f * 0.1f)) / 2, 0);
+			gameObject.GetComponent<BoxCollider2D>().size = new Vector2(WindowSize.WIDTH * 0.24f * 0.1f, 0.5f);
 			gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
 			// RigidBodyのアタッチメント
 			gameObject.AddComponent<Rigidbody2D>();
