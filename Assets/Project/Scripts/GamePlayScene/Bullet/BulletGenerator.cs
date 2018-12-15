8035bfebc1a9ffaf3520b1d40e8c299e72b47786
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2018-12-15 23:08:17 +0900

    Make it simple to input bullet parameters using enum.

--- Assets/Project/Scripts/GamePlayScene/Bullet/BulletGenerator.cs
+++ Assets/Project/Scripts/GamePlayScene/Bullet/BulletGenerator.cs
@@ -16,6 +16,38 @@ namespace Project.Scripts.GamePlayScene.Bullet
 		// Generatorが作成された時刻
 		public float startTime;
 
+		private enum Left
+		{
+			First = 1,
+			Second,
+			Third,
+			Fourth,
+			Fifth
+		}
+
+		private enum Right
+		{
+			First = 1,
+			Second,
+			Third,
+			Fourth,
+			Fifth
+		}
+
+		private enum Up
+		{
+			First = 1,
+			Second,
+			Third
+		}
+
+		private enum Down
+		{
+			First = 1,
+			Second,
+			Third
+		}
+
 		public void CreateBullets(int stageId)
 		{
 			List<IEnumerator> coroutines = new List<IEnumerator>();
@@ -23,24 +55,17 @@ namespace Project.Scripts.GamePlayScene.Bullet
 			switch (stageId)
 			{
 				case 1:
-					// 銃弾の初期位置
-					const int positionNumber = 1;
-					// 銃弾の移動ベクトル
-					Vector2 motionVector = new Vector2(-1.0f, 0.0f);
 					// 銃弾の出現時刻
 					const float appearanceTiming = 1.0f;
 					// 銃弾を作るインターバル
 					const float createInterval = 1.0f;
 					// coroutineのリスト
-					coroutines.Add(CreateBullet(positionNumber, motionVector, appearanceTiming, createInterval));
-					coroutines.Add(CreateBullet(positionNumber: 2,
-						motionVector: new Vector2(1.0f, 0.0f),
-						appearanceTiming: 2.0f, interval: 4.0f));
+					AddCoroutine(coroutines, Left.First, appearanceTiming, createInterval);
+					AddCoroutine(coroutine: coroutines, row: Right.Second, appearanceTiming: 2.0f,
+						createInterval: 4.0f);
 					break;
 				case 2:
-					coroutines.Add(CreateBullet(positionNumber: 5,
-						motionVector: new Vector2(1.0f, 0.0f),
-						appearanceTiming: 2.0f, interval: 4.0f));
+					AddCoroutine(coroutine: coroutines, row: Right.Fifth, appearanceTiming: 2.0f, createInterval: 4.0f);
 					break;
 				default:
 					throw new NotImplementedException();
@@ -50,13 +75,32 @@ namespace Project.Scripts.GamePlayScene.Bullet
 			foreach (IEnumerator coroutine in coroutines) StartCoroutine(coroutine);
 		}
 
+		// 銃弾の移動方向および出現させる位置を取得してCreatebullet()メソッドに引数を渡す
+		private void AddCoroutine(List<IEnumerator> coroutine, Left row, float appearanceTiming, float createInterval)
+		{
+			coroutine.Add(CreateBullet("left", (int) row, appearanceTiming, createInterval));
+		}
+
+		private void AddCoroutine(List<IEnumerator> coroutine, Right row, float appearanceTiming, float createInterval)
+		{
+			coroutine.Add(CreateBullet("right", (int) row, appearanceTiming, createInterval));
+		}
+
+		private void AddCoroutine(List<IEnumerator> coroutine, Up column, float appearanceTiming, float createInterval)
+		{
+			coroutine.Add(CreateBullet("up", (int) column, appearanceTiming, createInterval));
+		}
+
+		private void AddCoroutine(List<IEnumerator> coroutine, Down column, float appearanceTiming,
+			float createInterval)
+		{
+			coroutine.Add(CreateBullet("down", (int) column, appearanceTiming, createInterval));
+		}
+
 		// 指定した座標(x, y)に一定の時間間隔(interval)で銃弾を作成するメソッド
-		private IEnumerator CreateBullet(int positionNumber, Vector2 motionVector, float appearanceTiming,
+		private IEnumerator CreateBullet(String direction, int position, float appearanceTiming,
 			float interval)
 		{
-			// タイルの位置に合わせて銃弾の初期位置を設定する
-			Vector2 position = SetBulletPosition(positionNumber, motionVector);
-
 			var currentTime = Time.time;
 
 			// wait by the time the first bullet warning emerge
@@ -75,7 +119,7 @@ namespace Project.Scripts.GamePlayScene.Bullet
 				bullet.GetComponent<Renderer>().sortingLayerName = "Bullet";
 				// 変数の初期設定
 				NormalCartridgeController cartridgeScript = bullet.GetComponent<NormalCartridgeController>();
-				cartridgeScript.Initialize(position, motionVector);
+				cartridgeScript.Initialize(direction, position);
 
 				// emerge a bullet warning
 				GameObject warning = Instantiate(normalBulletWarningPrefab);
@@ -92,33 +136,6 @@ namespace Project.Scripts.GamePlayScene.Bullet
 				// 一定時間(interval)待つ
 				yield return new WaitForSeconds(appearanceTiming + interval * sum - (currentTime - startTime));
 			}
-		}
-
-		// positionNumber行またはpositionNumber列目のタイルを通過するように銃弾の初期位置を指定する
-		private static Vector2 SetBulletPosition(int positionNumber, Vector2 motionVector)
-		{
-			// 銃弾が左右方向に移動する場合
-			// 入力が(1<=positionNumber<=5)行であるか調べる
-			if ((motionVector.Equals(Vector2.left) || motionVector.Equals(Vector2.right)))
-			{
-				if (positionNumber < 1 || 5 < positionNumber)
-				{
-					throw new ArgumentOutOfRangeException("positionNumber", positionNumber, null);
-				}
-			}
-			// 銃弾が上下方向に移動する場合
-			// 入力が(1<=positionNumber<=3)列であるか調べる
-			else if ((motionVector.Equals(Vector2.up) || motionVector.Equals(Vector2.down)))
-			{
-				if (positionNumber < 1 || 3 < positionNumber)
-				{
-					throw new ArgumentOutOfRangeException("positionNumber", positionNumber, null);
-				}
-			}
-
-			return new Vector2(TileSize.WIDTH * (positionNumber - 2),
-				WindowSize.HEIGHT * 0.5f - (TileSize.MARGIN_TOP + TileSize.HEIGHT * 0.5f) -
-				TileSize.HEIGHT * (positionNumber - 1));
 		}
 	}
 }
