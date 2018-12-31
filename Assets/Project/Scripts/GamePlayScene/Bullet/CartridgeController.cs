81b2e5f74664d95cfca646f4baf05c55e26843ed
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2018-12-31 16:01:09 +0900

    Fix collision area (miscalculation)

--- Assets/Project/Scripts/GamePlayScene/Bullet/CartridgeController.cs
+++ Assets/Project/Scripts/GamePlayScene/Bullet/CartridgeController.cs
@@ -34,7 +34,7 @@ namespace Project.Scripts.GamePlayScene.Bullet
 			gameObject.GetComponent<BoxCollider2D>().offset =
 				new Vector2(-(1.5f - (WindowSize.WIDTH * 0.24f * 0.1f)) / 2, 0);
 			// 銃弾の先頭部分のみに当たり判定を与える
-			gameObject.GetComponent<BoxCollider2D>().size = new Vector2(TileSize.WIDTH - PanelSize.WIDTH, 0.5f);
+			gameObject.GetComponent<BoxCollider2D>().size = new Vector2((TileSize.WIDTH - PanelSize.WIDTH)/2, 0.5f);
 			gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
 			// RigidBodyのアタッチメント
 			gameObject.AddComponent<Rigidbody2D>();
