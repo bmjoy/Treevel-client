1b81e48e07295c64d7f19711e7fda8e0ad79861a
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2019-01-19 20:40:40 +0900

    Add HoleWarningController

---
+++ Assets/Project/Scripts/GamePlayScene/BulletWarning/HoleWarningController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project.Scripts.GamePlayScene.BulletWarning
+{
+	public class HoleWarningController : MonoBehaviour
+	{
+
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
