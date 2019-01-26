673dfafe03fa5fa171d89d285ba2bc50cf53fb83
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2019-01-26 23:02:37 +0900

    Delete magic number in warning

--- Assets/Project/Scripts/GamePlayScene/BulletWarning/NormalCartridgeWarningController.cs
+++ Assets/Project/Scripts/GamePlayScene/BulletWarning/NormalCartridgeWarningController.cs
@@ -9,8 +9,8 @@ namespace Project.Scripts.GamePlayScene.BulletWarning
 			float bulletWidth, float bulletHeight)
 		{
 			localScale = (float) (WindowSize.WIDTH * 0.15);
-			originalWidth = GetComponent<Renderer>().bounds.size.x;
-			originalHeight = GetComponent<Renderer>().bounds.size.y;
+			originalWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+			originalHeight = GetComponent<SpriteRenderer>().bounds.size.y;
 			transform.localScale *= new Vector2(localScale, localScale);
 			transform.position = bulletPosition + Vector2.Scale(bulletMotionVector,
 				                     new Vector2((bulletLocalScale * bulletWidth + localScale * originalWidth) / 2,
