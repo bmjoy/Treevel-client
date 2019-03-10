fc98ebf56d84d4c82cb44f727fc356c37cf4de40
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2019-03-10 11:01:09 +0900

    Delete unnecessary calculation

--- Assets/Project/Scripts/GamePlayScene/Bullet/NormalCartridgeController.cs
+++ Assets/Project/Scripts/GamePlayScene/Bullet/NormalCartridgeController.cs
@@ -1,12 +1,14 @@
 ﻿using Project.Scripts.Utils.Definitions;
+using UnityEngine;
 
 namespace Project.Scripts.GamePlayScene.Bullet
 {
 	public class NormalCartridgeController : CartridgeController
 	{
 		// コンストラクタがわりのメソッド
-		public override void Initialize(CartridgeDirection direction, int line)
+		public override void Initialize(CartridgeDirection direction, int line, Vector2 motionVector)
 		{
+			this.motionVector = motionVector;
 			SetInitialPosition(direction, line);
 		}
 	}
