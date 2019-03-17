694eaad79b4a06400c295a93dc97d1cae00d0393
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2019-03-17 12:17:37 +0900

    Move the Functions of HoleController.cs to NormalHoleController.cs

--- Assets/Project/Scripts/GamePlayScene/Bullet/NormalHoleController.cs
+++ Assets/Project/Scripts/GamePlayScene/Bullet/NormalHoleController.cs
@@ -1,15 +1,51 @@
-﻿using Project.Scripts.Utils.Definitions;
+﻿using System.Collections;
+using Project.Scripts.Utils.Definitions;
 using UnityEngine;
 
 namespace Project.Scripts.GamePlayScene.Bullet
 {
-	public class NormalHoleController : HoleController
+	public class NormalHoleController : BulletController
 	{
+		private int row;
+		private int column;
+
 		protected override void Awake()
 		{
 			base.Awake();
 			transform.localScale = new Vector2(HoleSize.WIDTH / originalWidth, HoleSize.HEIGHT / originalHeight) *
 			                       LOCAL_SCALE;
+		}
+
+		// コンストラクタがわりのメソッド
+		public void Initialize(int row, int column, Vector3 holeWarningPosition)
+		{
+			this.row = row;
+			this.column = column;
+			transform.position = holeWarningPosition;
+		}
+
+		protected override void OnFail()
+		{
+		}
+
+		// 当たり判定(holeの表示場所に、tileがあるかどうかを確認する)
+		public IEnumerator Delete()
+		{
+			var gamePlayDirector = FindObjectOfType<GamePlayDirector>();
+			var tile = GameObject.Find("Tile" + ((row - 1) * 3 + column));
+			// check whether panel exists on the tile
+			if (tile.transform.childCount != 0)
+			{
+				gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+				gamePlayDirector.Dispatch(GamePlayDirector.GameState.Failure);
+			}
+			else
+			{
+				// display the hole betweeen tile layer and panel layer
+				gameObject.GetComponent<Renderer>().sortingLayerName = "Hole";
+				yield return new WaitForSeconds(0.5f);
+				Destroy(gameObject);
+			}
 		}
 	}
 }
