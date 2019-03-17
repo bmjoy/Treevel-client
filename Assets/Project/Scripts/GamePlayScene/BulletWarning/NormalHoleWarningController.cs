d323f244a3cf090ce12a52dbeef55196fe253afb
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2019-03-17 12:29:57 +0900

    Move the Function of HoleWarningController.cs to NormalHoleWarningController.cs

--- Assets/Project/Scripts/GamePlayScene/BulletWarning/NormalHoleWarningController.cs
+++ Assets/Project/Scripts/GamePlayScene/BulletWarning/NormalHoleWarningController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace Project.Scripts.GamePlayScene.BulletWarning
 {
-	public class NormalHoleWarningController : HoleWarningController
+	public class NormalHoleWarningController : BulletWarningController
 	{
 		protected override void Awake()
 		{
@@ -13,7 +13,7 @@ namespace Project.Scripts.GamePlayScene.BulletWarning
 		}
 
 		// 警告のpositionを計算する
-		public override void Initialize(int row, int column)
+		public void Initialize(int row, int column)
 		{
 			const float topTilePositionY = WindowSize.HEIGHT * 0.5f - (TileSize.MARGIN_TOP + TileSize.HEIGHT * 0.5f);
 			transform.position =
