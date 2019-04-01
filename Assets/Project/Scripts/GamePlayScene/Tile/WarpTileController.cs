44dbd86062a34a024fa8845066e4f9199c91cbd8
高橋碧 <>
2019-04-01 23:02:41 +0900

    Create WarpTileController

---
+++ Assets/Project/Scripts/GamePlayScene/Tile/WarpTileController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Project.Scripts.GamePlayScene.Tile
+{
+	public class WarpTileController : TileController
+	{
+		// 相方のwarpTile
+		public GameObject pairTile;
+
+		protected void Initialize(Vector2 position, int tileNum, GameObject pairTile)
+		{
+			base.Initialize(position, tileNum);
+			this.pairTile = pairTile;
+		}
+	}
+}
