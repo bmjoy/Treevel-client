a91d26025ec7e851a3efa42f7a011378add2dbc1
高橋碧 <>
2019-04-06 16:34:24 +0900

    Create tiles

--- Assets/Project/Scripts/GamePlayScene/Tile/TileGenerator.cs
+++ Assets/Project/Scripts/GamePlayScene/Tile/TileGenerator.cs
@@ -6,42 +6,80 @@ namespace Project.Scripts.GamePlayScene.Tile
 	public class TileGenerator : MonoBehaviour
 	{
 		public GameObject normalTilePrefab;
+		public GameObject warpTilePrefab;
 
-		// 現段階では5行3列のタイル群
-		// 現段階ではステージ番号に依存しない
-		public void CreateTiles()
+		private readonly GameObject[,] tiles = new GameObject[5, 3];
+
+		/* 普通タイルの作成 */
+		public void CreateNormalTiles()
 		{
-			// 最上タイルのy座標
-			const float topTilePositionY = WindowSize.HEIGHT * 0.5f - (TileSize.MARGIN_TOP + TileSize.HEIGHT * 0.5f);
-			// タイルの識別番号（分かりやすさのため1から振り分け）
-			var tileNum = 1;
-			// タイルを格納する配列
-			GameObject[,] tiles = new GameObject[5, 3];
-
-			// タイルを作成し、配置する
-			for (var row = 0; row <= 4; row++)
+			for (var tileNum = 1; tileNum <= 15; tileNum++)
 			{
-				for (var column = -1; column <= 1; column++)
-				{
-					// 作成するタイルのx,y座標
-					var positionX = TileSize.WIDTH * column;
-					var positionY = topTilePositionY - TileSize.HEIGHT * row;
-					// 15枚のタイルに0-14の名前を付ける
-					tiles[row, column + 1] = CreateOneTile(new Vector2(positionX, positionY), tileNum);
-					tileNum++;
-				}
+				// 行 (0~4)
+				var row = (tileNum - 1) / 3;
+				// 列 (0~2)
+				var column = (tileNum - 1) % 3;
+
+				// 既に他タイルを作成している場合はスルー
+				if (tiles[row, column] != null) continue;
+
+				var tile = Instantiate(normalTilePrefab);
+
+				var tilePosition = GetTilePosition(tileNum);
+
+				tile.GetComponent<NormalTileController>().Initialize(tilePosition, tileNum);
+
+				SetTile(tileNum, tile);
 			}
 
 			// タイルの位置関係を作成する
 			MakeRelations(tiles);
 		}
 
-		private GameObject CreateOneTile(Vector2 position, int tileNum)
+		/* ワープタイルの作成 */
+		public void CreateWarpTiles(int firstTileNum, int secondTileNum)
 		{
-			var tile = Instantiate(normalTilePrefab);
-			tile.GetComponent<NormalTileController>().Initialize(position, tileNum);
+			var firstTile = Instantiate(warpTilePrefab);
+			var secondTile = Instantiate(warpTilePrefab);
 
-			return tile;
+			var firstTilePosition = GetTilePosition(firstTileNum);
+			var secondTilePosition = GetTilePosition(secondTileNum);
+
+			firstTile.GetComponent<WarpTileController>().Initialize(firstTilePosition, firstTileNum, secondTile);
+			secondTile.GetComponent<WarpTileController>().Initialize(secondTilePosition, secondTileNum, firstTile);
+
+			SetTile(firstTileNum, firstTile);
+			SetTile(secondTileNum, secondTile);
+		}
+
+		/* タイルの座標を取得 */
+		private static Vector2 GetTilePosition(int tileNum)
+		{
+			// 最上タイルのy座標
+			const float topTilePositionY = WindowSize.HEIGHT * 0.5f - (TileSize.MARGIN_TOP + TileSize.HEIGHT * 0.5f);
+
+			// 処理計算では0から扱いたい
+			tileNum -= 1;
+			// 行 (0~4)
+			var row = tileNum / 3;
+			// 列 (0~2)
+			var column = tileNum % 3;
+			// 作成するタイルのx,y座標
+			var positionX = TileSize.WIDTH * (column - 1);
+			var positionY = topTilePositionY - TileSize.HEIGHT * row;
+
+			return new Vector2(positionX, positionY);
+		}
+
+		/* タイルを配列に格納 */
+		private void SetTile(int tileNum, GameObject tile)
+		{
+			// 行 (0~4)
+			var row = (tileNum - 1) / 3;
+			// 列 (0~2)
+			var column = (tileNum - 1) % 3;
+
+			tiles[row, column] = tile;
 		}
 
 		private static void MakeRelations(GameObject[,] tiles)
