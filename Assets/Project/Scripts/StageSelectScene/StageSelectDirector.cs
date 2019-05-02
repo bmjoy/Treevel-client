c521ba39aef5dbe6dd17aa21fb9b7ed6226cedf0
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2019-05-02 13:19:58 +0900

    Add comments

--- Assets/Project/Scripts/StageSelectScene/StageSelectDirector.cs
+++ Assets/Project/Scripts/StageSelectScene/StageSelectDirector.cs
@@ -66,7 +66,10 @@ namespace Project.Scripts.StageSelectScene
 				button.GetComponent<Image>().color = SetButtonColor();
 				// Buttonの位置
 				var rectTransform = button.GetComponent<RectTransform>();
+				// 下部のマージン : 0.05f
+				// ボタン間の間隔 : 0.10f
 				var buttonPositionY = 0.05f + i * 0.10f;
+				// ボタンの縦幅 : 0.04f (上に0.02f, 下に0.02fをアンカー中央から伸ばす)
 				rectTransform.anchorMax = new Vector2(0.90f, buttonPositionY + 0.02f);
 				rectTransform.anchorMin = new Vector2(0.10f, buttonPositionY - 0.02f);
 				rectTransform.anchoredPosition = new Vector2(0.50f, buttonPositionY);
