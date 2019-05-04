262a3d05ffc200f2f2f3064bf538a801e80be70f
xiii111 <aoi.t19950517@gmail.com>
2019-05-04 14:52:24 +0900

    Put dummy background to hide back scene

--- Assets/Project/Scripts/StageSelectScene/StageSelectDirector.cs
+++ Assets/Project/Scripts/StageSelectScene/StageSelectDirector.cs
@@ -17,7 +17,7 @@ namespace Project.Scripts.StageSelectScene
 			MakeButtons();
 		}
 
-		protected static void StageButtonDown(GameObject clickedButton)
+		protected void StageButtonDown(GameObject clickedButton)
 		{
 			// タップされたステージidを取得（暫定的にボタンの名前）
 			var stageId = int.Parse(clickedButton.name);
@@ -26,8 +26,27 @@ namespace Project.Scripts.StageSelectScene
 			ss.IncChallengeNum(stageId);
 			// ステージ番号を渡す
 			GamePlayDirector.stageId = stageId;
+			// 背景画像をCanvasの下に置く
+			var canvas = GetCanvas().GetComponent<RectTransform>();;
+			var background = Instantiate(dummuBackgroundPrefab);
+			background.transform.SetParent(canvas, false);
 			// シーン遷移
 			SceneManager.LoadScene("GamePlayScene");
+		}
+
+		/* 現在アクティブなシーンの Canvas を取得 */
+		private static GameObject GetCanvas()
+		{
+			var canvas = new GameObject();
+			var scene = SceneManager.GetActiveScene();
+			foreach (var rootGameObject in scene.GetRootGameObjects())
+			{
+				if (rootGameObject.name == "Canvas")
+				{
+					canvas = rootGameObject;
+				}
+			}
+			return canvas;
 		}
 
 		/* ボタンの生成 */
