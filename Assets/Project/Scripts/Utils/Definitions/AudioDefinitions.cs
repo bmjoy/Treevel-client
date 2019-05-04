1cc48c0efe62e4e6d93d529b12d535312f8542d8
xiii111 <aoi.t19950517@gmail.com>
2019-05-04 18:04:24 +0900

    Add  string definitions

--- Assets/Project/Scripts/Utils/Definitions/AudioDefinitions.cs
+++ Assets/Project/Scripts/Utils/Definitions/AudioDefinitions.cs
@@ -4,4 +4,11 @@
 	{
 		public const float DEFAULT_VOLUME = 0.5f;
 	}
+
+	public static class ClipName
+	{
+		public const string PLAYING = "playing";
+		public const string SUCCESS = "success";
+		public const string FAILURE = "failure";
+	}
 }
--- Assets/Project/Scripts/Utils/PlayerPrefsUtils/PlayerPrefsKeys.cs
+++ Assets/Project/Scripts/Utils/PlayerPrefsUtils/PlayerPrefsKeys.cs
@@ -3,5 +3,6 @@
 	public static class PlayerPrefsKeys
 	{
 		public const string STAGE_STATUS_KEY = "STAGE_STATUS_KEY";
+		public const string VOLUME = "VOLUME";
 	}
 }
