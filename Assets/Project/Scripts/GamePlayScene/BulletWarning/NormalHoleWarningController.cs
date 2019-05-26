9639b115a2a31537a44e32c937d5cbedf5521390
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2019-05-26 23:06:03 +0900

    Implement random normal hole

--- Assets/Project/Scripts/GamePlayScene/BulletWarning/NormalHoleWarningController.cs
+++ Assets/Project/Scripts/GamePlayScene/BulletWarning/NormalHoleWarningController.cs
@@ -1,4 +1,5 @@
-﻿using Project.Scripts.Utils.Definitions;
+﻿using Project.Scripts.GamePlayScene.Bullet;
+using Project.Scripts.Utils.Definitions;
 using UnityEngine;
 
 namespace Project.Scripts.GamePlayScene.BulletWarning
@@ -13,8 +14,15 @@ namespace Project.Scripts.GamePlayScene.BulletWarning
 		}
 
 		// 警告のpositionを計算する
-		public void Initialize(int row, int column)
+		public void Initialize(ref int row, ref int column, BulletInfo bulletInfo)
 		{
+			if (row == (int) Row.Random && column == (int) Column.Random)
+			{
+				var tileNum = bulletInfo.GetTileNum();
+				row = (tileNum - 1) / StageSize.COLUMN + 1;
+				column = (tileNum - 1) % StageSize.COLUMN + 1;
+			}
+
 			const float topTilePositionY = WindowSize.HEIGHT * 0.5f - (TileSize.MARGIN_TOP + TileSize.HEIGHT * 0.5f);
 			transform.position =
 				new Vector2(TileSize.WIDTH * (column - 2), topTilePositionY - TileSize.HEIGHT * (row - 1));
