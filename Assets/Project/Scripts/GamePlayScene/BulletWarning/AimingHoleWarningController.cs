9185aaecf64ddf410de7e10454581e3d0ebc4876
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2019-05-26 23:06:35 +0900

    Implement random aiming hole

--- Assets/Project/Scripts/GamePlayScene/BulletWarning/AimingHoleWarningController.cs
+++ Assets/Project/Scripts/GamePlayScene/BulletWarning/AimingHoleWarningController.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections.Generic;
-using Project.Scripts.GamePlayScene.Bullet;
+﻿using Project.Scripts.GamePlayScene.Bullet;
 using Project.Scripts.Utils.Library;
-using UnityEngine;
 
 namespace Project.Scripts.GamePlayScene.BulletWarning
 {
@@ -11,13 +9,22 @@ namespace Project.Scripts.GamePlayScene.BulletWarning
 		{
 			// 撃ち抜くPanelを取得する
 			var aimingPanel = bulletInfo.GetAimingPanel();
-			var count = bulletInfo.GetCount();
-			var panelNum = aimingPanel[((count - 1) % aimingPanel.Length)];
+			int panelNum;
+			if (aimingPanel != null)
+			{
+				var count = bulletInfo.GetAimingHoleCount();
+				panelNum = aimingPanel[((count - 1) % aimingPanel.Length)];
+				// 次の表示位置を求める
+				bulletInfo.SetAimingHoleCount(count + 1);
+			}
+			else
+			{
+				panelNum = bulletInfo.GetNumberPanel();
+			}
+
 			var panel = PanelLibrary.GetPanel(panelNum);
 			// 警告の表示位置をPanelと同じ位置にする
 			transform.position = panel.transform.position;
-			// 次の表示位置を求める
-			bulletInfo.SetCount(count + 1);
 		}
 	}
 }
