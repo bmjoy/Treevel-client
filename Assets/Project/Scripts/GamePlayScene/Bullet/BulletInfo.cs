0c2cd34d1619bfba5337c29328a897f8dfb46f37
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2019-05-26 23:01:07 +0900

    Prepare general methods for random parameters

--- Assets/Project/Scripts/GamePlayScene/Bullet/BulletInfo.cs
+++ Assets/Project/Scripts/GamePlayScene/Bullet/BulletInfo.cs
@@ -4,6 +4,43 @@ namespace Project.Scripts.GamePlayScene.Bullet
 {
 	public class BulletInfo
 	{
+		// 配列の初期化を行うメソッド
+		private static int[] SetInitialRate(int arrayLength)
+		{
+			var returnArray = new int[arrayLength];
+			for (var index = 0; index < arrayLength; index++)
+			{
+				returnArray[index] = INITIAL_RATE;
+			}
+
+			return returnArray;
+		}
+
+		// 重みに基づき配列の何番目を選択するかを決定する(配列の最初は1番目)
+		private int GetRandomParameter(int[] randomParameters, int sumOfRandomParameters)
+		{
+			var randomValue = random.Next(sumOfRandomParameters) + 1;
+			var index = 0;
+			while (randomValue > 0)
+			{
+				randomValue -= randomParameters[index];
+				index += 1;
+			}
+
+			return index;
+		}
+
+		// int[]配列を更新し、その配列の要素和を求める
+		private void SetRandomParameter(out int[] randomParameters, out int sumOfRandomParameters,
+			int[] newRandomParameters)
+		{
+			randomParameters = newRandomParameters;
+			sumOfRandomParameters = 0;
+			foreach (var randomParameter in randomParameters)
+			{
+				sumOfRandomParameters += randomParameter;
+			}
+		}
 		// TurnCartridgeの曲がる方向
 		private int[] turnDirection = null;
 
