59f959222a691fad11c31aec16ce54d3abb8085e
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2019-06-17 22:23:51 +0900

    Control BulletGroup in BulletGroupGenerator

--- Assets/Project/Scripts/GamePlayScene/Bullet/BulletGroupGenerator.cs
+++ Assets/Project/Scripts/GamePlayScene/Bullet/BulletGroupGenerator.cs
@@ -12,10 +12,13 @@ namespace Project.Scripts.GamePlayScene.Bullet
 		// 生成された銃弾のID(sortingOrder)
 		private short bulletId = -32768;
 
+		// 銃弾グループを制御するcoroutine
 		private List<IEnumerator> coroutines;
 
 		private GamePlayDirector gamePlayDirector;
 
+		// 銃弾グループのprefab
+		public GameObject bulletGroupControllerPrefab;
 
 		// Generatorが作成された時刻
 		public float startTime;
@@ -32,15 +35,25 @@ namespace Project.Scripts.GamePlayScene.Bullet
 			GamePlayDirector.OnFail -= OnFail;
 		}
 
+		/* 一連の銃弾の生成タイミングを管理するBulletGroupを作成する */
+		public void CreateBulletGroups(List<IEnumerator> coroutines)
 		{
 			this.coroutines = coroutines;
 			gamePlayDirector = FindObjectOfType<GamePlayDirector>();
 			startTime = Time.time;
 
-			foreach (var coroutine in coroutines) StartCoroutine(coroutine);
+			foreach (var coroutine in this.coroutines) StartCoroutine(coroutine);
 		}
 
+		/* BulletGroupを生成する */
+		public IEnumerator CreateBulletGroup(float appearanceTime, float interval, bool loop,
+			List<GameObject> bulletGenerators)
 		{
+			var bulletGroup = Instantiate(bulletGroupControllerPrefab);
+			var bulletGroupScript = bulletGroup.GetComponent<BulletGroupController>();
+			bulletGroupScript.Initialize(startTime: startTime, appearanceTime: appearanceTime, interval: interval,
+				loop: loop, bulletGenerators: bulletGenerators);
+			yield return StartCoroutine(bulletGroupScript.CreateBullets());
 		}
 
 		{
