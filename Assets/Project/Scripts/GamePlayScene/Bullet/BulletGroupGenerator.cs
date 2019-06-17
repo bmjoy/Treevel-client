53fec9a4ee588ee1d9af7c037a1b5e54888ff8a0
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2019-06-17 23:24:23 +0900

    Control bullet generator prefabs in BulletGroupGenerator

--- Assets/Project/Scripts/GamePlayScene/Bullet/BulletGroupGenerator.cs
+++ Assets/Project/Scripts/GamePlayScene/Bullet/BulletGroupGenerator.cs
@@ -20,6 +20,12 @@ namespace Project.Scripts.GamePlayScene.Bullet
 		// 銃弾グループのprefab
 		public GameObject bulletGroupControllerPrefab;
 
+		// 各銃弾のGeneratorのprefab
+		public GameObject normalCartridgeGeneratorPrefab;
+		public GameObject turnCartridgeGeneratorPrefab;
+		public GameObject normalHoleGeneratorPrefab;
+		public GameObject aimingHoleGeneratorPrefab;
+
 		// Generatorが作成された時刻
 		public float startTime;
 
@@ -65,30 +71,118 @@ namespace Project.Scripts.GamePlayScene.Bullet
 		{
 		}
 
+		/* NormalCartridgeのGeneratorを生成する */
+		public GameObject CreateNormalCartridgeGenerator(int ratio, CartridgeDirection cartridgeDirection, Row row)
 		{
+			var cartridgeGenerator = Instantiate(normalCartridgeGeneratorPrefab);
+			var cartridgeGeneratorScript = cartridgeGenerator.GetComponent<NormalCartridgeGenerator>();
+			cartridgeGeneratorScript.Initialize(ratio, cartridgeDirection, row);
+			return cartridgeGenerator;
 		}
 
+		public GameObject CreateNormalCartridgeGenerator(int ratio, CartridgeDirection cartridgeDirection,
+			Column column)
 		{
+			var cartridgeGenerator = Instantiate(normalCartridgeGeneratorPrefab);
+			var cartridgeGeneratorScript = cartridgeGenerator.GetComponent<NormalCartridgeGenerator>();
+			cartridgeGeneratorScript.Initialize(ratio, cartridgeDirection, column);
+			return cartridgeGenerator;
 		}
 
+		public GameObject CreateNormalCartridgeGenerator(int ratio, CartridgeDirection cartridgeDirection, Row row,
+			int[] randomCartridgeDirection, int[] randomRow, int[] randomColumn)
 		{
-
+			var cartridgeGenerator = Instantiate(normalCartridgeGeneratorPrefab);
+			var cartridgeGeneratorScript = cartridgeGenerator.GetComponent<NormalCartridgeGenerator>();
+			cartridgeGeneratorScript.Initialize(ratio, cartridgeDirection, row, randomCartridgeDirection, randomRow,
+				randomColumn);
+			return cartridgeGenerator;
 		}
 
+		public GameObject CreateNormalCartridgeGenerator(int ratio, CartridgeDirection cartridgeDirection,
+			Column column, int[] randomCartridgeDirection, int[] randomRow, int[] randomColumn)
 		{
-
-
-
-
+			var cartridgeGenerator = Instantiate(normalCartridgeGeneratorPrefab);
+			var cartridgeGeneratorScript = cartridgeGenerator.GetComponent<NormalCartridgeGenerator>();
+			cartridgeGeneratorScript.Initialize(ratio, cartridgeDirection, column, randomCartridgeDirection, randomRow,
+				randomColumn);
+			return cartridgeGenerator;
 		}
 
+		/* TurnCartridgeのGeneratorを生成する*/
+		public GameObject CreateTurnCartridgeGenerator(int ratio, CartridgeDirection cartridgeDirection, Row row,
+			int[] turnDirection = null, int[] turnLine = null)
 		{
+			var cartridgeGenerator = Instantiate(turnCartridgeGeneratorPrefab);
+			var cartridgeGeneratorScript = cartridgeGenerator.GetComponent<TurnCartridgeGenerator>();
+			cartridgeGeneratorScript.Initialize(ratio, cartridgeDirection, row);
+			return cartridgeGenerator;
 		}
 
+		public GameObject CreateTurnCartridgeGenerator(int ratio, CartridgeDirection cartridgeDirection, Column column,
+			int[] turnDirection = null, int[] turnLine = null)
 		{
+			var cartridgeGenerator = Instantiate(turnCartridgeGeneratorPrefab);
+			var cartridgeGeneratorScript = cartridgeGenerator.GetComponent<TurnCartridgeGenerator>();
+			cartridgeGeneratorScript.Initialize(ratio, cartridgeDirection, column);
+			return cartridgeGenerator;
 		}
 
+		public GameObject CreateTurnCartridgeGenerator(int ratio, CartridgeDirection cartridgeDirection, Row row,
+			int[] turnDirection, int[] turnLine, int[] randomTurnDirections, int[] randomTurnRow,
+			int[] randomTurnColumn)
 		{
+			var cartridgeGenerator = Instantiate(turnCartridgeGeneratorPrefab);
+			var cartridgeGeneratorScript = cartridgeGenerator.GetComponent<TurnCartridgeGenerator>();
+			cartridgeGeneratorScript.Initialize(ratio, cartridgeDirection, row, randomTurnDirections, randomTurnRow,
+				randomTurnColumn);
+			return cartridgeGenerator;
+		}
+
+		public GameObject CreateTurnCartridgeGenerator(int ratio, CartridgeDirection cartridgeDirection, Column column,
+			int[] turnDirection, int[] turnLine, int[] randomTurnDirections, int[] randomTurnRow,
+			int[] randomTurnColumn)
+		{
+			var cartridgeGenerator = Instantiate(turnCartridgeGeneratorPrefab);
+			var cartridgeGeneratorScript = cartridgeGenerator.GetComponent<TurnCartridgeGenerator>();
+			cartridgeGeneratorScript.Initialize(ratio, cartridgeDirection, column, randomTurnDirections, randomTurnRow,
+				randomTurnColumn);
+			return cartridgeGenerator;
+		}
+
+		/* NormalHoleのGeneratorを生成する */
+		public GameObject CreateNormalHoleGenerator(int ratio, Row row, Column column)
+		{
+			var holeGenerator = Instantiate(normalHoleGeneratorPrefab);
+			var holeGeneratorScript = holeGenerator.GetComponent<NormalHoleGenerator>();
+			holeGeneratorScript.Initialize(ratio, row, column);
+			return holeGenerator;
+		}
+
+		public GameObject CreateNormalHoleGenerator(int ratio, Row row, Column column, int[] randomRow,
+			int[] randomColumn)
+		{
+			var holeGenerator = Instantiate(normalHoleGeneratorPrefab);
+			var holeGeneratorScript = holeGenerator.GetComponent<NormalHoleGenerator>();
+			holeGeneratorScript.Initialize(ratio, row, column, randomRow, randomColumn);
+			return holeGenerator;
+		}
+
+		/* AimingHoleのGeneratorを生成する */
+		public GameObject CreateAimingHoleGenerator(int ratio, int[] aimingPanel = null)
+		{
+			var holeGenerator = Instantiate(aimingHoleGeneratorPrefab);
+			var holeGeneratorScript = holeGenerator.GetComponent<AimingHoleGenerator>();
+			holeGeneratorScript.Initialize(ratio, aimingPanel);
+			return holeGenerator;
+		}
+
+		public GameObject CreateAimingHoleGenerator(int ratio, int[] aimingPanel, int[] randomNumberPanel)
+		{
+			var holeGenerator = Instantiate(aimingHoleGeneratorPrefab);
+			var holeGeneratorScript = holeGenerator.GetComponent<AimingHoleGenerator>();
+			holeGeneratorScript.Initialize(ratio, aimingPanel, randomNumberPanel);
+			return holeGenerator;
 		}
 	}
 }
