a7a8b65ba21376db2723a47206e2fd9a1a4962c4
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2019-07-28 18:27:43 +0900

    Move a const value to Definitions

--- Assets/Project/Scripts/Utils/Definitions/BulletDefinitions.cs
+++ Assets/Project/Scripts/Utils/Definitions/BulletDefinitions.cs
@@ -1,5 +1,13 @@
 ﻿namespace Project.Scripts.Utils.Definitions
 {
+    public static class BulletGeneratorParameter
+    {
+        /// <summary>
+        /// ランダムな値を決めるときの各要素の重みの初期値
+        /// </summary>
+        public const int INITIAL_RATIO = 100;
+    }
+    
     public enum ECartridgeType {
         Normal = 1,
         Turn,
