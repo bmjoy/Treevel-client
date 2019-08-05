c371d5703864927695e6eba25012c2cc2d7b8a9b
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2019-08-05 21:03:08 +0900

    Add comments in root scripts of bullets

--- Assets/Project/Scripts/GamePlayScene/Bullet/BulletController.cs
+++ Assets/Project/Scripts/GamePlayScene/Bullet/BulletController.cs
@@ -7,10 +7,18 @@ namespace Project.Scripts.GamePlayScene.Bullet
     // Bulletに共通したフィールド、メソッドの定義
     public abstract class BulletController : MonoBehaviour
     {
+        /// <summary>
+        /// 銃弾の大きさ
+        /// </summary>
         [NonSerialized] protected const float LOCAL_SCALE = 1.0f;
 
-        // 元画像のサイズ
+        /// <summary>
+        /// 元画像の横幅
+        /// </summary>
         [NonSerialized] public float originalWidth;
+        /// <summary>
+        /// 元画像の縦幅
+        /// </summary>
         [NonSerialized] public float originalHeight;
 
         protected virtual void Awake()
--- Assets/Project/Scripts/GamePlayScene/Bullet/BulletGenerator.cs
+++ Assets/Project/Scripts/GamePlayScene/Bullet/BulletGenerator.cs
@@ -9,7 +9,7 @@ namespace Project.Scripts.GamePlayScene.Bullet
     {
         protected GamePlayDirector gamePlayDirector;
 
-        // このGeneratorの出現する重み
+        // Generatorの出現する確率
         [NonSerialized] public int ratio = BulletGeneratorParameter.INITIAL_RATIO;
 
         protected void Awake()
@@ -29,7 +29,44 @@ namespace Project.Scripts.GamePlayScene.Bullet
             GamePlayDirector.OnFail -= OnFail;
         }
 
+<<<<<<< HEAD
         /* 実際に1つの銃弾を生成する方法を各銃弾のGenerator毎に実装する */
+=======
+        /* 配列の初期化を行うメソッド */
+        // 必ずライブラリ化する
+        protected static int[] SetInitialRatio(int arrayLength)
+        {
+            var returnArray = new int[arrayLength];
+            for (var index = 0; index < arrayLength; index++) {
+                returnArray[index] = INITIAL_RATIO;
+            }
+
+            return returnArray;
+        }
+
+        /* 重みに基づき配列の何番目を選択するかをランダムに決定する(配列の最初であるならば0を返す) */
+        // 必ずライブラリ化する
+        public static int GetRandomParameter(int[] randomParameters)
+        {
+            var sumOfRandomParameters = randomParameters.Sum();
+            // 1以上重みの総和以下の値をランダムに取得する
+            var randomValue = new System.Random().Next(sumOfRandomParameters) + 1;
+            var index = 0;
+            // 重み配列の最初の要素から順に、ランダムな値から値を引く
+            while (randomValue > 0) {
+                randomValue -= randomParameters[index];
+                index += 1;
+            }
+
+            return index - 1;
+        }
+
+        /// <summary>
+        /// 1つの銃弾の生成を行う
+        /// </summary>
+        /// <param name="bulletId"> 銃弾の描画順序を決めるID</param>
+        /// <returns></returns>
+>>>>>>> Add comments in root scripts of bullets
         public abstract IEnumerator CreateBullet(int bulletId);
 
         private void OnSucceed()
