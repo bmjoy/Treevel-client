fce249b8c9952323c6cd418b926de622edfb6b1d
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2019-08-07 02:57:02 +0900

    Add comments in Warning scripts

--- Assets/Project/Scripts/GamePlayScene/BulletWarning/AimingHoleWarningController.cs
+++ Assets/Project/Scripts/GamePlayScene/BulletWarning/AimingHoleWarningController.cs
@@ -4,6 +4,11 @@ namespace Project.Scripts.GamePlayScene.BulletWarning
 {
     public class AimingHoleWarningController : NormalHoleWarningController
     {
+        /// <summary>
+        /// 座標を設定する
+        /// </summary>
+        /// <param name="aimingPanel"> 撃ち抜くNumberPanelの番号配列 </param>
+        /// <param name="aimingHoleCount"> aimingPanel配列の何番目を参照するか</param>
         public void Initialize(int[] aimingPanel, ref int aimingHoleCount)
         {
             var panelNum = aimingPanel[((aimingHoleCount - 1) % aimingPanel.Length)];
