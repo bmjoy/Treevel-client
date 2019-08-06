a5efcb65936d5c4d3af5d6fe67157a59802a084b
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2019-08-07 02:57:30 +0900

    Add comments in BulletDefinition

--- Assets/Project/Scripts/Utils/Definitions/BulletDefinitions.cs
+++ Assets/Project/Scripts/Utils/Definitions/BulletDefinitions.cs
@@ -8,18 +8,27 @@
         public const int INITIAL_RATIO = 100;
     }
 
+    /// <summary>
+    /// 弾丸型の銃弾の名前
+    /// </summary>
     public enum ECartridgeType {
         Normal = 1,
         Turn,
         Random = -1
     }
 
+    /// <summary>
+    /// 銃痕型の銃弾の名前
+    /// </summary>
     public enum EHoleType {
         Normal = 1,
         Aiming,
         Random = -1
     }
 
+    /// <summary>
+    /// 銃弾の移動方向
+    /// </summary>
     public enum ECartridgeDirection {
         ToLeft = 1,
         ToRight,
@@ -28,6 +37,9 @@
         Random = -1
     }
 
+    /// <summary>
+    /// 盤面の行番号
+    /// </summary>
     public enum ERow {
         First = 1,
         Second,
@@ -37,6 +49,9 @@
         Random = -1
     }
 
+    /// <summary>
+    /// 盤面の列番号
+    /// </summary>
     public enum EColumn {
         Left = 1,
         Center,
