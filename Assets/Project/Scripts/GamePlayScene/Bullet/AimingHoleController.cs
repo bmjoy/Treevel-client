d3e973ed88209e181d855d3e9c7d669da698635c
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2019-08-07 02:49:25 +0900

    Add comments in Aiming scripts

--- Assets/Project/Scripts/GamePlayScene/Bullet/AimingHoleController.cs
+++ Assets/Project/Scripts/GamePlayScene/Bullet/AimingHoleController.cs
@@ -6,7 +6,7 @@ namespace Project.Scripts.GamePlayScene.Bullet
     public class AimingHoleController : NormalHoleController
     {
         /// <summary>
-        /// 銃弾の座標を警告の座標に設定
+        /// 銃弾の座標を警告の座標に設定する
         /// </summary>
         /// <param name="holeWarningPosition"> 警告の座標 </param>
         public void Initialize(Vector2 holeWarningPosition)
