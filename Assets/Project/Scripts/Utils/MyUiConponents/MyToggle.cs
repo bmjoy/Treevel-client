8aa94c1882aff1a89432b79765f24190e0e09c6f
xiii111 <aoi.t19950517@gmail.com>
2019-08-08 22:34:40 +0900

    Create MyToggle inheriting Toggle

---
+++ Assets/Project/Scripts/Utils/MyUiConponents/MyToggle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Project.Scripts.Utils.MyUiConponents
+{
+    public class MyToggle : Toggle
+    {
+        /// <summary>
+        /// ToggleGroup の AllowSwitchOff=false を再現
+        /// <see cref="Toggle.OnPointerClick"/>.
+        /// </summary>
+        public override void OnPointerClick(PointerEventData eventData)
+        {
+            if (eventData.button != PointerEventData.InputButton.Left) return;
+
+            if (!IsActive() || !IsInteractable())
+                return;
+
+            // ON 状態のものを OFF にすることは許さない
+            if (!isOn) isOn = true;
+        }
+    }
+}
