f938153b091f7e523600a9b212460a6bd5dc562c
xiii111 <aoi.t19950517@gmail.com>
2019-09-01 19:34:38 +0900

    Auto format

--- Assets/ThirdParties/NestedScrollRect/NestedScrollRect.cs
+++ Assets/ThirdParties/NestedScrollRect/NestedScrollRect.cs
@@ -4,45 +4,54 @@ using UnityEngine.UI;
 using System;
 using UnityEngine.EventSystems;
 
-public class NestedScrollRect : ScrollRect {
+public class NestedScrollRect : ScrollRect
+{
 
     private bool routeToParent = false;
 
-    public override void OnInitializePotentialDrag (PointerEventData eventData)
+    public override void OnInitializePotentialDrag(PointerEventData eventData)
     {
-        transform.DoParentEventSystemHandler<IInitializePotentialDragHandler>((parent) => { parent.OnInitializePotentialDrag(eventData); });
-        base.OnInitializePotentialDrag (eventData);
+        transform.DoParentEventSystemHandler<IInitializePotentialDragHandler>((parent) => {
+            parent.OnInitializePotentialDrag(eventData);
+        });
+        base.OnInitializePotentialDrag(eventData);
     }
 
-    public override void OnDrag (UnityEngine.EventSystems.PointerEventData eventData)
+    public override void OnDrag(UnityEngine.EventSystems.PointerEventData eventData)
     {
-        if(routeToParent)
-            transform.DoParentEventSystemHandler<IDragHandler>((parent) => { parent.OnDrag(eventData); });
+        if (routeToParent)
+            transform.DoParentEventSystemHandler<IDragHandler>((parent) => {
+            parent.OnDrag(eventData);
+        });
         else
-            base.OnDrag (eventData);
+            base.OnDrag(eventData);
     }
 
-    public override void OnBeginDrag (UnityEngine.EventSystems.PointerEventData eventData)
+    public override void OnBeginDrag(UnityEngine.EventSystems.PointerEventData eventData)
     {
-        if(!horizontal && Math.Abs (eventData.delta.x) > Math.Abs (eventData.delta.y))
+        if (!horizontal && Math.Abs(eventData.delta.x) > Math.Abs(eventData.delta.y))
             routeToParent = true;
-        else if(!vertical && Math.Abs (eventData.delta.x) < Math.Abs (eventData.delta.y))
+        else if (!vertical && Math.Abs(eventData.delta.x) < Math.Abs(eventData.delta.y))
             routeToParent = true;
         else
             routeToParent = false;
 
-        if(routeToParent)
-            transform.DoParentEventSystemHandler<IBeginDragHandler>((parent) => { parent.OnBeginDrag(eventData); });
+        if (routeToParent)
+            transform.DoParentEventSystemHandler<IBeginDragHandler>((parent) => {
+            parent.OnBeginDrag(eventData);
+        });
         else
-            base.OnBeginDrag (eventData);
+            base.OnBeginDrag(eventData);
     }
 
-    public override void OnEndDrag (UnityEngine.EventSystems.PointerEventData eventData)
+    public override void OnEndDrag(UnityEngine.EventSystems.PointerEventData eventData)
     {
-        if(routeToParent)
-            transform.DoParentEventSystemHandler<IEndDragHandler>((parent) => { parent.OnEndDrag(eventData); });
+        if (routeToParent)
+            transform.DoParentEventSystemHandler<IEndDragHandler>((parent) => {
+            parent.OnEndDrag(eventData);
+        });
         else
-            base.OnEndDrag (eventData);
+            base.OnEndDrag(eventData);
         routeToParent = false;
     }
 }
