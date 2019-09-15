8ad43b5895ff84bed73cb4e6cd04e0c2046c0f51
takasusuke <aimaru.kei.lock@ezweb.ne.jp>
2019-09-15 20:55:55 +0900

    Reformat code

--- Assets/Project/Scripts/GamePlayScene/Bullet/NormalHoleController.cs
+++ Assets/Project/Scripts/GamePlayScene/Bullet/NormalHoleController.cs
@@ -104,14 +104,11 @@ namespace Project.Scripts.GamePlayScene.Bullet
             // 銃痕(hole)が出現したフレーム以外では衝突を考えない
             if (transform.position.z < 0) return;
             // 数字パネルとの衝突
-            if (other.gameObject.CompareTag(TagName.NUMBER_PANEL))
-            {
+            if (other.gameObject.CompareTag(TagName.NUMBER_PANEL)) {
                 // 衝突したオブジェクトは赤色に変える
                 gameObject.GetComponent<SpriteRenderer>().color = Color.red;
                 gameObject.transform.parent = other.transform;
-            }
-            else if(other.gameObject.CompareTag(TagName.DUMMY_PANEL))
-            {
+            } else if (other.gameObject.CompareTag(TagName.DUMMY_PANEL)) {
                 gameObject.transform.parent = other.transform;
             }
         }
