3c0bbdd5f9a7bfe79d3d6e70ab2fb51e0484a194
xiii111 <aoi.t19950517@gmail.com>
2019-11-04 03:17:04 +0900

    Add DoNotShow settings

---
+++ Assets/Project/Scripts/ConfigScene/DoNotShowController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Project.Scripts.Utils.Definitions;
+using Project.Scripts.Utils.PlayerPrefsUtils;
+
+namespace Project.Scripts.ConfigScene
+{
+    public class DoNotShowController : MonoBehaviour
+    {
+        /// <summary>
+        /// 概要を表示するかのトグル
+        /// </summary>
+        private Toggle _doNotShowToggle;
+
+        private void Awake()
+        {
+            _doNotShowToggle = GetComponent<Toggle>();
+            _doNotShowToggle.isOn = PlayerPrefs.GetInt(PlayerPrefsKeys.DO_NOT_SHOW, 0) == 1;
+            _doNotShowToggle.GetComponent<Toggle>().onValueChanged.AddListener(delegate
+            {
+                ToggleValueChanged(_doNotShowToggle.GetComponent<Toggle>());
+            });
+        }
+
+        /// <summary>
+        /// 概要を表示するかのトグルが押されたときの処理
+        /// </summary>
+        private static void ToggleValueChanged(Toggle toggle)
+        {
+            PlayerPrefs.SetInt(PlayerPrefsKeys.DO_NOT_SHOW, toggle.isOn ? 1 : 0);
+        }
+    }
+}
