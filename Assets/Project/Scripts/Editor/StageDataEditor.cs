930ca8008d382864e87e5f4b5ff2c6b03eb4fbcd
takesi0627 <woody70825@gmail.com>
2019-11-04 10:20:41 +0900

    StageDataEditor refactoring

--- Assets/Project/Scripts/Editor/StageDataEditor.cs
+++ Assets/Project/Scripts/Editor/StageDataEditor.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEditor;
 using Project.Scripts.GameDatas;
 using Project.Scripts.Utils.Definitions;
+using System;
 
 [CustomEditor(typeof(StageData))]
 [CanEditMultipleObjects]
@@ -21,6 +22,20 @@ public class StageDataEditor : Editor
         EditorGUI.BeginChangeCheck();
 
         EditorGUILayout.PropertyField(serializedObject.FindProperty("id"));
+
+        DrawPanelList();
+
+        DrawBulletGroupList();
+
+        // Set object dirty, this will make it be saved after saving the project.
+        if (EditorGUI.EndChangeCheck()) {
+            EditorUtility.SetDirty(serializedObject.targetObject);
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private void DrawPanelList()
+    {
         _panelDatasProp.isExpanded = EditorGUILayout.Foldout(_panelDatasProp.isExpanded, new GUIContent("Panels"));
         EditorGUILayout.PropertyField(_panelDatasProp.FindPropertyRelative("Array.size"));
         if (_panelDatasProp.isExpanded) {
@@ -58,90 +73,95 @@ public class StageDataEditor : Editor
                 }
             }
         }
+    }
+    private void DrawBulletGroupList ()
+    {
+        DrawArrayProperty(_bulletGroupDatasProp, (bulletGroupDataProp, index) => {
+            bulletGroupDataProp.isExpanded = EditorGUILayout.Foldout(bulletGroupDataProp.isExpanded, $"Bullet Group {index + 1}");
+            if (bulletGroupDataProp.isExpanded) {
+                EditorGUI.indentLevel++;
 
-        _bulletGroupDatasProp.isExpanded = EditorGUILayout.Foldout(_bulletGroupDatasProp.isExpanded, new GUIContent("Bullet Groups"));
-        EditorGUILayout.PropertyField(_bulletGroupDatasProp.FindPropertyRelative("Array.size"));
-        if (_bulletGroupDatasProp.isExpanded) {
-            for (int i = 0 ; i < _bulletGroupDatasProp.arraySize ; i++) {
-                SerializedProperty bulletGroupDataProp = _bulletGroupDatasProp.GetArrayElementAtIndex(i);
+                EditorGUILayout.PropertyField(bulletGroupDataProp.FindPropertyRelative("appearTime"));
+                EditorGUILayout.PropertyField(bulletGroupDataProp.FindPropertyRelative("interval"));
+                EditorGUILayout.PropertyField(bulletGroupDataProp.FindPropertyRelative("loop"));
 
-                bulletGroupDataProp.isExpanded = EditorGUILayout.Foldout(bulletGroupDataProp.isExpanded, $"Bullet Group {i + 1}");
-                if (bulletGroupDataProp.isExpanded) {
-                    EditorGUI.indentLevel++;
+                SerializedProperty bulletListProp = bulletGroupDataProp.FindPropertyRelative("bullets");
+                DrawArrayProperty(bulletListProp, (bulletDataProp, index2) => {
+                    bulletDataProp.isExpanded = EditorGUILayout.Foldout(bulletDataProp.isExpanded, $"Bullet {index2 + 1}");
+                    if (bulletDataProp.isExpanded) {
+                        SerializedProperty bulletTypeProp = bulletDataProp.FindPropertyRelative("type");
+                        SerializedProperty directionProp = bulletDataProp.FindPropertyRelative("direction");
+                        SerializedProperty lineProp = bulletDataProp.FindPropertyRelative("line");
+                        bulletTypeProp.enumValueIndex = (int)(EBulletType)EditorGUILayout.EnumPopup(new GUIContent("Type"), (EBulletType)bulletTypeProp.enumValueIndex);
 
-                    EditorGUILayout.PropertyField(bulletGroupDataProp.FindPropertyRelative("appearTime"));
-                    EditorGUILayout.PropertyField(bulletGroupDataProp.FindPropertyRelative("interval"));
-                    EditorGUILayout.PropertyField(bulletGroupDataProp.FindPropertyRelative("loop"));
+                        EditorGUILayout.PropertyField(bulletDataProp.FindPropertyRelative("ratio"));
+                        switch ((EBulletType)bulletTypeProp.enumValueIndex) {
+                            case EBulletType.NormalCartridge: {
+                                    directionProp.intValue = (int)(ECartridgeDirection)EditorGUILayout.EnumPopup(
+                                        label: new GUIContent("Direction"),
+                                        selected: (ECartridgeDirection)directionProp.intValue,
+                                        checkEnabled: (eType) => { // ランダムは選択不能にする
+                                            return (ECartridgeDirection)eType != ECartridgeDirection.Random;
+                                        },
+                                        includeObsolete: false
+                                    );
 
-                    SerializedProperty bulletListProp = bulletGroupDataProp.FindPropertyRelative("bullets");
-
-                    bulletListProp.isExpanded = EditorGUILayout.Foldout(bulletListProp.isExpanded, "Bullets");
-                    EditorGUILayout.PropertyField(bulletListProp.FindPropertyRelative("Array.size"));
-                    if (bulletListProp.isExpanded) {
-                        for (int j = 0; j < bulletListProp.arraySize; j++) {
-                            SerializedProperty bulletDataProp = bulletListProp.GetArrayElementAtIndex(i);
-
-                            bulletDataProp.isExpanded = EditorGUILayout.Foldout(bulletDataProp.isExpanded, $"Bullet {j + 1}");
-                            if (bulletDataProp.isExpanded) {
-                                SerializedProperty bulletTypeProp = bulletDataProp.FindPropertyRelative("type");
-                                SerializedProperty directionProp = bulletDataProp.FindPropertyRelative("direction");
-                                SerializedProperty lineProp = bulletDataProp.FindPropertyRelative("line");
-                                bulletTypeProp.enumValueIndex = (int)(EBulletType)EditorGUILayout.EnumPopup(new GUIContent("Type"), (EBulletType)bulletTypeProp.enumValueIndex);
-
-                                EditorGUILayout.PropertyField(bulletDataProp.FindPropertyRelative("ratio"));
-                                switch ((EBulletType)bulletTypeProp.enumValueIndex) {
-                                    case EBulletType.NormalCartridge: {
-                                            directionProp.intValue = (int)(ECartridgeDirection)EditorGUILayout.EnumPopup(
-                                                label: new GUIContent("Direction"),
-                                                selected: (ECartridgeDirection)directionProp.intValue,
-                                                checkEnabled: (eType) => { // ランダムは選択不能にする
-                                                    return (ECartridgeDirection)eType != ECartridgeDirection.Random;
-                                                },
-                                                includeObsolete: false
-                                            );
-
-                                            switch ((ECartridgeDirection)directionProp.intValue) {
-                                                case ECartridgeDirection.ToLeft:
-                                                case ECartridgeDirection.ToRight:
-                                                    lineProp.intValue = (int)(ERow)EditorGUILayout.EnumPopup(new GUIContent("Row"), (ERow)lineProp.intValue);
-                                                    break;
-                                                case ECartridgeDirection.ToBottom:
-                                                case ECartridgeDirection.ToUp:
-                                                    lineProp.intValue = (int)(EColumn)EditorGUILayout.EnumPopup(new GUIContent("Column"), (EColumn)lineProp.intValue);
-                                                    break;
-                                            }
+                                    switch ((ECartridgeDirection)directionProp.intValue) {
+                                        case ECartridgeDirection.ToLeft:
+                                        case ECartridgeDirection.ToRight:
+                                            lineProp.intValue = (int)(ERow)EditorGUILayout.EnumPopup(new GUIContent("Row"), (ERow)lineProp.intValue);
                                             break;
-                                        }
-                                    case EBulletType.RandomCartridge: {
-                                        directionProp.intValue = (int)(ECartridgeDirection.Random);
-                                        directionProp.intValue = (int)(ECartridgeDirection)EditorGUILayout.EnumPopup(
-                                                label: new GUIContent("Direction"),
-                                                selected: (ECartridgeDirection)directionProp.intValue,
-                                                checkEnabled: (eType) => {
-                                                    return (ECartridgeDirection)eType == ECartridgeDirection.Random;
-                                                },
-                                                includeObsolete: false
-                                            );
-                                        EditorGUILayout.PropertyField(bulletDataProp.FindPropertyRelative("randomCartridgeDirection"));
-                                        EditorGUILayout.PropertyField(bulletDataProp.FindPropertyRelative("randomRow"));
-                                        EditorGUILayout.PropertyField(bulletDataProp.FindPropertyRelative("randomColumn"));
-
-                                        break;
+                                        case ECartridgeDirection.ToBottom:
+                                        case ECartridgeDirection.ToUp:
+                                            lineProp.intValue = (int)(EColumn)EditorGUILayout.EnumPopup(new GUIContent("Column"), (EColumn)lineProp.intValue);
+                                            break;
                                     }
+                                    break;
                                 }
+                            case EBulletType.RandomNormalCartridge: {
+                                directionProp.intValue = (int)(ECartridgeDirection.Random);
+                                directionProp.intValue = (int)(ECartridgeDirection)EditorGUILayout.EnumPopup(
+                                        label: new GUIContent("Direction"),
+                                        selected: (ECartridgeDirection)directionProp.intValue,
+                                        checkEnabled: (eType) => {
+                                            return (ECartridgeDirection)eType == ECartridgeDirection.Random;
+                                        },
+                                        includeObsolete: false
+                                    );
+                                DrawArrayProperty(bulletDataProp.FindPropertyRelative("randomCartridgeDirection"));
+                                DrawArrayProperty(bulletDataProp.FindPropertyRelative("randomRow"));
+                                DrawArrayProperty(bulletDataProp.FindPropertyRelative("randomColumn"));
+                                break;
                             }
                         }
                     }
+                });
+                EditorGUI.indentLevel--;
+            }
+        });
+    }
 
-                    EditorGUI.indentLevel--;
+    /// <summary>
+    /// List、Arrayなどの配列型のPropertyを描画するメソッド
+    /// </summary>
+    /// <param name="property">描画する対象、配列である必要がある</param>
+    /// <param name="action">配列に格納する要素に対する描画処理、nullの場合はEditorGUI.PropertyFieldを使う</param>
+    private static void DrawArrayProperty(SerializedProperty property, Action<SerializedProperty, int> action = null)
+    {
+        if (!property.isArray)
+            return;
+
+        property.isExpanded = EditorGUILayout.Foldout(property.isExpanded, property.displayName);
+        EditorGUILayout.PropertyField(property.FindPropertyRelative("Array.size"));
+        if (property.isExpanded) {
+            for (int i = 0 ; i < property.arraySize ; ++i) {
+                var arrayElementProperty = property.GetArrayElementAtIndex(i);
+                if (action != null) {
+                    action.Invoke(arrayElementProperty, i);
+                } else {
+                    EditorGUILayout.PropertyField(arrayElementProperty, new GUIContent(arrayElementProperty.displayName));
                 }
             }
         }
-
-        // Set object dirty, this will make it be saved after saving the project.
-        if (EditorGUI.EndChangeCheck()) {
-            EditorUtility.SetDirty(serializedObject.targetObject);
-        }
-        serializedObject.ApplyModifiedProperties();
     }
 }
