782ef7e9190cfefac6bbdf6b7c1ae9e8e9cb1dd9
takesi0627 <woody70825@gmail.com>
2020-03-18 13:01:29 +0900

    create ProgressBar and UIManager class

---
+++ Assets/Project/Scripts/MenuSelectScene/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using Project.Scripts.UIComponents;
+using Project.Scripts.Utils.Patterns;
+using UnityEngine;
+
+namespace Project.Scripts.MenuSelectScene
+{
+    /// <summary>
+    /// 全体ゲームに出現するUI（プログレスバー、メッセージダイアログ等）
+    /// を制御するマネージャークラス
+    /// </summary>
+    public class UIManager : SingletonObject<UIManager>
+    {
+        /// <summary>
+        /// プログレスバーのインスタンス
+        /// </summary>
+        public ProgressBar ProgressBar { get; private set; }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            ProgressBar = FindObjectOfType<ProgressBar>();
+        }
+
+        /// <summary>
+        /// プログレスバーを表示／非表示
+        /// </summary>
+        /// <param name="show"></param>
+        public void ShowProgressBar(bool show)
+        {
+            ProgressBar.gameObject.SetActive(show);
+        }
+    }
+}
---
+++ Assets/Project/Scripts/UIComponents/ProgressBar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Project.Scripts.UIComponents
+{
+    public class ProgressBar : MonoBehaviour
+    {
+        /// <summary>
+        /// プログレス
+        /// </summary>
+        [Range(0f, 100f)]
+        private float _progress;
+
+
+        /// <summary>
+        /// プログレスを表示するテキスト
+        /// </summary>
+        [SerializeField] private Image _progressImage;
+
+        public float Progress {
+            get => _progress;
+            set {
+                if (_progress.Equals(value))
+                    return;
+                _progress = value;
+                _progressImage.fillAmount = _progress / 100f;
+            }
+        }
+
+        private void Awake()
+        {
+            Progress = 0;
+        }
+    }
+}
