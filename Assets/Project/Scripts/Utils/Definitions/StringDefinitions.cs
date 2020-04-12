32f1dffc29180f7f0ec51892240ccc5b08886ae7
xiii111 <aoi.t19950517@gmail.com>
2020-04-12 21:49:49 +0900

    [Refactoring] Remove unused string definitions

--- Assets/Project/Scripts/Utils/Definitions/StringDefinitions.cs
+++ Assets/Project/Scripts/Utils/Definitions/StringDefinitions.cs
@@ -6,16 +6,11 @@
     public static class SceneName
     {
         public const string MENU_SELECT_SCENE = "MenuSelectScene";
-        public const string LEVEL_SELECT_SCENE = "LevelSelectScene";
         public const string SPRING_STAGE_SELECT_SCENE = "SpringStageSelectScene";
         public const string SUMMER_STAGE_SELECT_SCENE = "SummerStageSelectScene";
         public const string AUTOMN_STAGE_SELECT_SCENE = "AutomnStageSelectScene";
         public const string WINTER_STAGE_SELECT_SCENE = "WinterStageSelectScene";
         public const string GAME_PLAY_SCENE = "GamePlayScene";
-        public const string GAME_PAUSE_SCENE = "GamePauseScene";
-        public const string RECORD_SCENE = "RecordScene";
-        public const string TUTORIAL_SCENE = "TutorialScene";
-        public const string SETTINGS_SCENE = "SettingsScene";
     }
 
     /// <summary>
