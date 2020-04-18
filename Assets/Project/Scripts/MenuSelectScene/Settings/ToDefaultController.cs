ad56e50a9e863fc0ca9b9fa23065df169b7383a6
xiii111 <aoi.t19950517@gmail.com>
2020-04-18 21:34:05 +0900

    Set button action in inspector

--- Assets/Project/Scripts/MenuSelectScene/Settings/ToDefaultController.cs
+++ Assets/Project/Scripts/MenuSelectScene/Settings/ToDefaultController.cs
@@ -17,20 +17,9 @@ namespace Project.Scripts.MenuSelectScene.Settings
         public static event ChangeAction OnUpdate;
 
         /// <summary>
-        /// デフォルトに戻すボタン
-        /// </summary>
-        private Button _toDefaultButton;
-
-        private void Awake()
-        {
-            _toDefaultButton = GetComponent<Button>();
-            _toDefaultButton.onClick.AddListener(ToDefaultButtonDown);
-        }
-
-        /// <summary>
         /// デフォルトに戻すボタンを押した場合の処理
         /// </summary>
-        private static void ToDefaultButtonDown()
+        public void ToDefaultButtonDown()
         {
             PlayerPrefs.DeleteKey(PlayerPrefsKeys.LANGUAGE);
             PlayerPrefs.DeleteKey(PlayerPrefsKeys.BGM_VOLUME);
