a68147257c0572cc50120d80e651fc26d0282351
takesi0627 <woody70825@gmail.com>
2020-04-30 21:13:33 +0900

    add document

--- Assets/Project/Scripts/GamePlayScene/Bottle/LifeDummyBottleController.cs
+++ Assets/Project/Scripts/GamePlayScene/Bottle/LifeDummyBottleController.cs
@@ -1,5 +1,8 @@
 ﻿namespace Project.Scripts.GamePlayScene.Bottle
 {
+    /// <summary>
+    /// ライフ付き、成功判定なし、フリック可能なボトル
+    /// </summary>
     public class LifeDummyBottleController : DynamicBottleController
     {
         public override void Initialize(GameDatas.BottleData bottleData)
