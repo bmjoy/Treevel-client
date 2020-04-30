6e1432333de9835766f61f4871a6d45eb4da13e4
Keisuke Yamakawa <aimaru.kei.lock@ezweb.ne.jp>
2020-05-01 03:36:20 +0900

    Show status bar for Android

---
+++ Assets/Project/Scripts/UIComponents/StatusBarController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Project.Scripts.UIComponents
+{
+    /// <summary>
+    /// ステータスバーの表示を行う
+    /// </summary>
+    class StatusBarController
+    {
+        private static int _FLAG_FULLSCREEN = 0x00000400;
+        private static int _FLAG_FORCE_NOT_FULLSCREEN = 0x00000800;
+
+        public static void Show()
+        {
+            // フルスクリーン表示をやめる 
+            using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+            {
+                using (var activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+                {
+                    // 別スレッド
+                    activity.Call("runOnUiThread", new AndroidJavaRunnable(RunOnUiThread));
+                }
+            }
+        }
+
+        /// <summary>
+        /// フルスクリーン表示をやめるフラッグを管理する
+        /// </summary>
+        private static void RunOnUiThread()
+        {
+            using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+            {
+                using (var activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+                {
+                    using (var window = activity.Call<AndroidJavaObject>("getWindow"))
+                    {
+                        window.Call("clearFlags", _FLAG_FULLSCREEN);
+                        window.Call("addFlags", _FLAG_FORCE_NOT_FULLSCREEN);
+                    }
+                }
+            }
+        }
+    }
+}
