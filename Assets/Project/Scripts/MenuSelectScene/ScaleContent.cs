c5e9bbd4e3320f3fb684bb3f6b8b5c7542e30941
Keisuke Yamakawa <aimaru.kei.lock@ezweb.ne.jp>
2020-05-06 17:29:10 +0900

    Write namespace

--- Assets/Project/Scripts/MenuSelectScene/ScaleContent.cs
+++ Assets/Project/Scripts/MenuSelectScene/ScaleContent.cs
@@ -5,213 +5,216 @@ using UnityEngine;
 using UnityEngine.UI;
 using Project.Scripts.UIComponents;
 
-public class ScaleContent : MonoBehaviour
-{
-    private ScrollRect _scrollRect;
-    private TransformGesture _transformGesture;
-    [SerializeField] private GameObject _content;
-    private RectTransform _contentRect;
-
-    /// <summary>
-    /// タッチした2点
-    /// </summary>
-    private Vector2 _prePoint1;
-    private Vector2 _prePoint2;
-
-    /// <summary>
-    /// タッチした2点間の距離
-    /// </summary>
-    private float _preScreenDist = 0.0f;
-
-    /// <summary>
-    /// タッチした2点の中点
-    /// </summary>
-    private Vector3 _preMeanPoint;
-
-    /// <summary>
-    /// 拡大率 (初期値 : 1.0f)
-    /// </summary>
-    private float _preScale = 1.0f;
-    /// <summary>
-    /// 拡大率の上限
-    /// </summary>
-    private const float _SCALE_MAX = 1.50f;
-    /// <summary>
-    /// 拡大率の下限
-    /// </summary>
-    private const float _SCALE_MIN = 0.5f;
-
-    /// <summary>
-    /// Contentの余白(Screen何個分の余白があるか)
-    /// </summary>
-    private static float _LEFT_OFFSET;
-    private static float _RIGHT_OFFSET;
-    private static float _TOP_OFFSET;
-    private static float _BOTTOM_OFFSET;
-
-    /// <summary>
-    /// 補正後のキャンバスの大きさ
-    /// </summary>
-    private Vector2 _scaledCanvas;
-
-    private void Awake()
+namespace Project.Scripts.MenuSelectScene
     {
-        _scrollRect = GetComponent<ScrollRect>();
-        _transformGesture = GetComponent<TransformGesture>();
-        _scaledCanvas = GameObject.Find("LevelSelect/Canvas").GetComponent<RectTransform>().sizeDelta;
-        _contentRect = _content.GetComponent<RectTransform>();
-        
-        ExpandContent();
-        // Contentの余白を取得
-        _LEFT_OFFSET = Mathf.Abs(_contentRect.anchorMin.x - _contentRect.pivot.x);
-        _RIGHT_OFFSET = _contentRect.anchorMax.x - _contentRect.pivot.x;
-        _TOP_OFFSET = _contentRect.anchorMax.y - _contentRect.pivot.y;
-        _BOTTOM_OFFSET = Mathf.Abs(_contentRect.anchorMin.y - _contentRect.pivot.y);
-        // 初期位置の調整
-        _contentRect.transform.localPosition += new Vector3(0, _scaledCanvas.y / 2, 0);
-    }
-
-    private void OnEnable()
+    public class ScaleContent : MonoBehaviour
     {
-        // 2点のタッチ開始時
-        _transformGesture.TransformStarted += OnTransformStarted;
-        // 2点のタッチ中
-        _transformGesture.Transformed += OnTransformed;
-        // 2点のタッチ終了時
-        _transformGesture.TransformCompleted += OnTransformCompleted;
-    }
+        private ScrollRect _scrollRect;
+        private TransformGesture _transformGesture;
+        [SerializeField] private GameObject _content;
+        private RectTransform _contentRect;
 
-    private void OnDisable()
-    {
-        _transformGesture.TransformStarted -= OnTransformStarted;
-        _transformGesture.Transformed -= OnTransformed;
-        _transformGesture.TransformCompleted -= OnTransformCompleted;
-    }
+        /// <summary>
+        /// タッチした2点
+        /// </summary>
+        private Vector2 _prePoint1;
+        private Vector2 _prePoint2;
 
-    private void OnTransformStarted(object sender, EventArgs e)
-    {
-        // タッチ位置を取得
-        _prePoint1 = _transformGesture.ActivePointers[0].Position;
-        _prePoint2 = _transformGesture.ActivePointers[1].Position;
-        // スクリーン上の距離を計算
-        _preScreenDist = Vector2.Distance(_prePoint1, _prePoint2);
-        // スクリーン上の中点座標を計算
-        _preMeanPoint = (_prePoint1 + _prePoint2) / 2;
-        // 2点タッチしている時はスクロールしない
-        _scrollRect.enabled = false;
-    }
+        /// <summary>
+        /// タッチした2点間の距離
+        /// </summary>
+        private float _preScreenDist = 0.0f;
 
-    private void OnTransformed(object sender, EventArgs e)
-    {
-        // タッチ位置等の計算
-        var _newPoint1 = _transformGesture.ActivePointers[0].Position;
-        var _newPoint2 = _transformGesture.ActivePointers[1].Position;
-        var _newScreenDist = Vector2.Distance(_newPoint1, _newPoint2);
-        var _newMeanPoint = (Vector3)((_newPoint1 + _newPoint2) / 2);
+        /// <summary>
+        /// タッチした2点の中点
+        /// </summary>
+        private Vector3 _preMeanPoint;
 
-        // 2点のタッチ開始時の距離と現在の距離の比から拡大率を求める
-        var _newScale = _preScale * _newScreenDist / _preScreenDist;
-        // 拡大率を閾値内に抑える
-        _newScale = Mathf.Clamp(_newScale, _SCALE_MIN, _SCALE_MAX);
-        // Contentの拡大縮小
-        _content.GetComponent<RectTransform>().localScale = new Vector2(_newScale, _newScale);
+        /// <summary>
+        /// 拡大率 (初期値 : 1.0f)
+        /// </summary>
+        private float _preScale = 1.0f;
+        /// <summary>
+        /// 拡大率の上限
+        /// </summary>
+        private const float _SCALE_MAX = 1.50f;
+        /// <summary>
+        /// 拡大率の下限
+        /// </summary>
+        private const float _SCALE_MIN = 0.5f;
 
-        // 拡大縮小前の中点を拡大縮小後の中点に合わせるようにContentの平行移動量を求める
-        var _preContentPoint = ConvertFromScreenToContent(_preMeanPoint, _preScale);
-        var _newContentPoint = ConvertFromScreenToContent(_newMeanPoint, _preScale);
-        // Content空間での2点の差分
-        var moveAmount = _newContentPoint - _preContentPoint * _newScale / _preScale;
-        moveAmount = KeepInContent(moveAmount, _newScale);
-        // Contentの平行移動
-        _content.GetComponent<RectTransform>().transform.localPosition += new Vector3(moveAmount.x, moveAmount.y, 0);
+        /// <summary>
+        /// Contentの余白(Screen何個分の余白があるか)
+        /// </summary>
+        private static float _LEFT_OFFSET;
+        private static float _RIGHT_OFFSET;
+        private static float _TOP_OFFSET;
+        private static float _BOTTOM_OFFSET;
 
-        // 値の更新
-        _prePoint1 = _newPoint1;
-        _prePoint2 = _newPoint2;
-        _preScreenDist = _newScreenDist;
-        _preMeanPoint = _newMeanPoint;
-        _preScale = _newScale;
-    }
+        /// <summary>
+        /// 補正後のキャンバスの大きさ
+        /// </summary>
+        private Vector2 _scaledCanvas;
 
-    /// <summary>
-    /// スクリーン空間の座標をContent空間の座標に変換する
-    /// </summary>
-    /// <param name="screenPoint"> スクリーン上の座標 </param>
-    /// <param name="scale"> Contentの拡大率 </param>
-    /// <returns> Content空間の座標 </returns>
-    private Vector3 ConvertFromScreenToContent(Vector3 screenCoordinate, float scale)
-    {
-        return ((-1) * _content.GetComponent<RectTransform>().transform.localPosition + (screenCoordinate - new Vector3(_scaledCanvas.x / 2, _scaledCanvas.y / 2))) / scale;
-    }
-
-    /// <summary>
-    /// 平行移動後にContentの外を表示しないようにする
-    /// </summary>
-    /// <param name="moveAmount"> 平行移動量 </param>
-    /// <param name="scale"> Contentの拡大率 </param>
-    /// <returns> 平行移動量 </returns>
-    private Vector2 KeepInContent(Vector2 moveAmount, float scale)
-    {
-        // 平行移動前のスクリーン中心のContent空間での座標
-        var _preLocalPosition = _content.GetComponent<RectTransform>().transform.localPosition;
-
-        // Contentの左端のチェック
-        var leftLimit = (_LEFT_OFFSET * scale - 0.5f) * _scaledCanvas.x;
-        if (_preLocalPosition.x + moveAmount.x >= leftLimit)
+        private void Awake()
         {
-            moveAmount.x = leftLimit - _preLocalPosition.x;
-        }
-        // Contentの右端のチェック
-        var rightLimit = (-1) * ((_RIGHT_OFFSET * scale - 0.5f) * _scaledCanvas.x);
-        if (_preLocalPosition.x + moveAmount.x <= rightLimit)
-        {
-            moveAmount.x = rightLimit - _preLocalPosition.x;
-        }
-        // Contentの上端のチェック
-        var topLimit = (-1) * ((_TOP_OFFSET * scale - 0.5f) * _scaledCanvas.y);
-        if (_preLocalPosition.y + moveAmount.y <= topLimit)
-        {
-            moveAmount.y = topLimit - _preLocalPosition.y;
-        }
-        // Contentの下端のチェック
-        var bottomLimit = (_BOTTOM_OFFSET * scale - 0.5f) * _scaledCanvas.y;
-        if (_preLocalPosition.y + moveAmount.y >= bottomLimit)
-        {
-            moveAmount.y = bottomLimit - _preLocalPosition.y;
+            _scrollRect = GetComponent<ScrollRect>();
+            _transformGesture = GetComponent<TransformGesture>();
+            _scaledCanvas = GameObject.Find("LevelSelect/Canvas").GetComponent<RectTransform>().sizeDelta;
+            _contentRect = _content.GetComponent<RectTransform>();
+            
+            ExpandContent();
+            // Contentの余白を取得
+            _LEFT_OFFSET = Mathf.Abs(_contentRect.anchorMin.x - _contentRect.pivot.x);
+            _RIGHT_OFFSET = _contentRect.anchorMax.x - _contentRect.pivot.x;
+            _TOP_OFFSET = _contentRect.anchorMax.y - _contentRect.pivot.y;
+            _BOTTOM_OFFSET = Mathf.Abs(_contentRect.anchorMin.y - _contentRect.pivot.y);
+            // 初期位置の調整
+            _contentRect.transform.localPosition += new Vector3(0, _scaledCanvas.y / 2, 0);
         }
 
-        return moveAmount;
-    }
-
-    private void OnTransformCompleted(object sender, EventArgs e)
-    {
-        // スクロール制限を解除する
-        _scrollRect.enabled = true;
-    }
-
-    /// <summary>
-    /// ContentのサイズをSafeAreaの分だけ拡大する
-    /// </summary>
-    private void ExpandContent()
-    {
-        // ContentのサイズをSafeAreaの分だけ拡大する
-        var beforeAnchorMin = _contentRect.anchorMin;
-        var beforeAnchorMax = _contentRect.anchorMax;
-        var (anchorMin, anchorMax) = SafeAreaPanel.GetSafeAreaAnchor();
-        _contentRect.anchorMin -= anchorMin;
-        _contentRect.anchorMax += (Vector2.one - anchorMax);
-        // Contentの拡大率
-        var scale = (beforeAnchorMax - beforeAnchorMin) / (_contentRect.anchorMax - _contentRect.anchorMin);
-
-        if(scale == Vector2.one) return;
-        // Content内の全オブジェクトのanchor位置の調整
-        var margin = anchorMin / (_contentRect.anchorMax - _contentRect.anchorMin);
-        foreach (var tree in _content.GetComponentsInChildren<Transform>().Where(t => t != _content.transform).Select(t => t.gameObject))
+        private void OnEnable()
         {
-            var re = tree.GetComponent<RectTransform>();
-            re.anchorMin *= scale;
-            re.anchorMin += margin;
-            re.anchorMax = re.anchorMin;
+            // 2点のタッチ開始時
+            _transformGesture.TransformStarted += OnTransformStarted;
+            // 2点のタッチ中
+            _transformGesture.Transformed += OnTransformed;
+            // 2点のタッチ終了時
+            _transformGesture.TransformCompleted += OnTransformCompleted;
+        }
+
+        private void OnDisable()
+        {
+            _transformGesture.TransformStarted -= OnTransformStarted;
+            _transformGesture.Transformed -= OnTransformed;
+            _transformGesture.TransformCompleted -= OnTransformCompleted;
+        }
+
+        private void OnTransformStarted(object sender, EventArgs e)
+        {
+            // タッチ位置を取得
+            _prePoint1 = _transformGesture.ActivePointers[0].Position;
+            _prePoint2 = _transformGesture.ActivePointers[1].Position;
+            // スクリーン上の距離を計算
+            _preScreenDist = Vector2.Distance(_prePoint1, _prePoint2);
+            // スクリーン上の中点座標を計算
+            _preMeanPoint = (_prePoint1 + _prePoint2) / 2;
+            // 2点タッチしている時はスクロールしない
+            _scrollRect.enabled = false;
+        }
+
+        private void OnTransformed(object sender, EventArgs e)
+        {
+            // タッチ位置等の計算
+            var _newPoint1 = _transformGesture.ActivePointers[0].Position;
+            var _newPoint2 = _transformGesture.ActivePointers[1].Position;
+            var _newScreenDist = Vector2.Distance(_newPoint1, _newPoint2);
+            var _newMeanPoint = (Vector3)((_newPoint1 + _newPoint2) / 2);
+
+            // 2点のタッチ開始時の距離と現在の距離の比から拡大率を求める
+            var _newScale = _preScale * _newScreenDist / _preScreenDist;
+            // 拡大率を閾値内に抑える
+            _newScale = Mathf.Clamp(_newScale, _SCALE_MIN, _SCALE_MAX);
+            // Contentの拡大縮小
+            _content.GetComponent<RectTransform>().localScale = new Vector2(_newScale, _newScale);
+
+            // 拡大縮小前の中点を拡大縮小後の中点に合わせるようにContentの平行移動量を求める
+            var _preContentPoint = ConvertFromScreenToContent(_preMeanPoint, _preScale);
+            var _newContentPoint = ConvertFromScreenToContent(_newMeanPoint, _preScale);
+            // Content空間での2点の差分
+            var moveAmount = _newContentPoint - _preContentPoint * _newScale / _preScale;
+            moveAmount = KeepInContent(moveAmount, _newScale);
+            // Contentの平行移動
+            _content.GetComponent<RectTransform>().transform.localPosition += new Vector3(moveAmount.x, moveAmount.y, 0);
+
+            // 値の更新
+            _prePoint1 = _newPoint1;
+            _prePoint2 = _newPoint2;
+            _preScreenDist = _newScreenDist;
+            _preMeanPoint = _newMeanPoint;
+            _preScale = _newScale;
+        }
+
+        /// <summary>
+        /// スクリーン空間の座標をContent空間の座標に変換する
+        /// </summary>
+        /// <param name="screenPoint"> スクリーン上の座標 </param>
+        /// <param name="scale"> Contentの拡大率 </param>
+        /// <returns> Content空間の座標 </returns>
+        private Vector3 ConvertFromScreenToContent(Vector3 screenCoordinate, float scale)
+        {
+            return ((-1) * _content.GetComponent<RectTransform>().transform.localPosition + (screenCoordinate - new Vector3(_scaledCanvas.x / 2, _scaledCanvas.y / 2))) / scale;
+        }
+
+        /// <summary>
+        /// 平行移動後にContentの外を表示しないようにする
+        /// </summary>
+        /// <param name="moveAmount"> 平行移動量 </param>
+        /// <param name="scale"> Contentの拡大率 </param>
+        /// <returns> 平行移動量 </returns>
+        private Vector2 KeepInContent(Vector2 moveAmount, float scale)
+        {
+            // 平行移動前のスクリーン中心のContent空間での座標
+            var _preLocalPosition = _content.GetComponent<RectTransform>().transform.localPosition;
+
+            // Contentの左端のチェック
+            var leftLimit = (_LEFT_OFFSET * scale - 0.5f) * _scaledCanvas.x;
+            if (_preLocalPosition.x + moveAmount.x >= leftLimit)
+            {
+                moveAmount.x = leftLimit - _preLocalPosition.x;
+            }
+            // Contentの右端のチェック
+            var rightLimit = (-1) * ((_RIGHT_OFFSET * scale - 0.5f) * _scaledCanvas.x);
+            if (_preLocalPosition.x + moveAmount.x <= rightLimit)
+            {
+                moveAmount.x = rightLimit - _preLocalPosition.x;
+            }
+            // Contentの上端のチェック
+            var topLimit = (-1) * ((_TOP_OFFSET * scale - 0.5f) * _scaledCanvas.y);
+            if (_preLocalPosition.y + moveAmount.y <= topLimit)
+            {
+                moveAmount.y = topLimit - _preLocalPosition.y;
+            }
+            // Contentの下端のチェック
+            var bottomLimit = (_BOTTOM_OFFSET * scale - 0.5f) * _scaledCanvas.y;
+            if (_preLocalPosition.y + moveAmount.y >= bottomLimit)
+            {
+                moveAmount.y = bottomLimit - _preLocalPosition.y;
+            }
+
+            return moveAmount;
+        }
+
+        private void OnTransformCompleted(object sender, EventArgs e)
+        {
+            // スクロール制限を解除する
+            _scrollRect.enabled = true;
+        }
+
+        /// <summary>
+        /// ContentのサイズをSafeAreaの分だけ拡大する
+        /// </summary>
+        private void ExpandContent()
+        {
+            // ContentのサイズをSafeAreaの分だけ拡大する
+            var beforeAnchorMin = _contentRect.anchorMin;
+            var beforeAnchorMax = _contentRect.anchorMax;
+            var (anchorMin, anchorMax) = SafeAreaPanel.GetSafeAreaAnchor();
+            _contentRect.anchorMin -= anchorMin;
+            _contentRect.anchorMax += (Vector2.one - anchorMax);
+            // Contentの拡大率
+            var scale = (beforeAnchorMax - beforeAnchorMin) / (_contentRect.anchorMax - _contentRect.anchorMin);
+
+            if(scale == Vector2.one) return;
+            // Content内の全オブジェクトのanchor位置の調整
+            var margin = anchorMin / (_contentRect.anchorMax - _contentRect.anchorMin);
+            foreach (var tree in _content.GetComponentsInChildren<Transform>().Where(t => t != _content.transform).Select(t => t.gameObject))
+            {
+                var re = tree.GetComponent<RectTransform>();
+                re.anchorMin *= scale;
+                re.anchorMin += margin;
+                re.anchorMax = re.anchorMin;
+            }
         }
     }
 }
