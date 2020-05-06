8db014aacaec945f42e94a7aa1fd6a3c68dd67bb
takesi0627 <woody70825@gmail.com>
2020-05-07 00:54:38 +0900

    remove using

--- Assets/Project/Scripts/StartUpScene/StartUpDirector.cs
+++ Assets/Project/Scripts/StartUpScene/StartUpDirector.cs
@@ -4,7 +4,6 @@ using Project.Scripts.MenuSelectScene;
 using Project.Scripts.Utils;
 using Project.Scripts.Utils.Definitions;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.SceneManagement;
 
