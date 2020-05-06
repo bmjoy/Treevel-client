003badd5fbdf85488fbbf57540957500aa97c986
takesi0627 <woody70825@gmail.com>
2020-05-07 00:54:11 +0900

    add namespace

--- Assets/Project/Scripts/StartUpScene/StartUpDirector.cs
+++ Assets/Project/Scripts/StartUpScene/StartUpDirector.cs
@@ -8,36 +8,39 @@ using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.SceneManagement;
 
-public class StartUpDirector : MonoBehaviour
+namespace Project.Scripts.StartUpScene
 {
-    IEnumerator Start()
+    public class StartUpDirector : MonoBehaviour
     {
-        // UIManager Initialize
-        yield return new WaitWhile(() => !UIManager.Instance.Initialized);
+        IEnumerator Start()
+        {
+            // UIManager Initialize
+            yield return new WaitWhile(() => !UIManager.Instance.Initialized);
 
-        // AAS Initialize
-        AddressableAssetManager.Initalize().Completed += OnAASInitializeCompleted;
+            // AAS Initialize
+            AddressableAssetManager.Initalize().Completed += OnAASInitializeCompleted;
 
-        // Database Initialize
-        GameDataBase.Initialize();
+            // Database Initialize
+            GameDataBase.Initialize();
 
-        // TODO Network Initialize
-    }
+            // TODO Network Initialize
+        }
 
-    /// <summary>
-    /// AASが初期化された直後に行うべきこと。
-    /// 1. MenuSelectSceneを読み込む
-    /// 2. MenuSelectSceneが読み込んだ直後にStartUpSceneをアンロード
-    /// </summary>
-    private void OnAASInitializeCompleted(AsyncOperationHandle handle)
-    {
-        var menuSelectSceneHandler = AddressableAssetManager.LoadScene(SceneName.MENU_SELECT_SCENE, LoadSceneMode.Additive);
-        menuSelectSceneHandler.Completed += async(handle2) => {
-            // TODO remove before merged
-            await Task.Delay(1000);
+        /// <summary>
+        /// AASが初期化された直後に行うべきこと。
+        /// 1. MenuSelectSceneを読み込む
+        /// 2. MenuSelectSceneが読み込んだ直後にStartUpSceneをアンロード
+        /// </summary>
+        private void OnAASInitializeCompleted(AsyncOperationHandle handle)
+        {
+            var menuSelectSceneHandler = AddressableAssetManager.LoadScene(SceneName.MENU_SELECT_SCENE, LoadSceneMode.Additive);
+            menuSelectSceneHandler.Completed += async(handle2) => {
+                // TODO remove before merged
+                await Task.Delay(1000);
 
-            // Unload Startup Scene
-            SceneManager.UnloadSceneAsync(SceneName.START_UP_SCENE);
-        };
+                // Unload Startup Scene
+                SceneManager.UnloadSceneAsync(SceneName.START_UP_SCENE);
+            };
+        }
     }
 }
