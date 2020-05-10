1579021892db8266f5913ae004e118d6ebb81635
Keisuke Yamakawa <aimaru.kei.lock@ezweb.ne.jp>
2020-05-10 20:16:56 +0900

    place UI considering status bar

--- Assets/Project/Scripts/UIComponents/SafeAreaPanel.cs
+++ Assets/Project/Scripts/UIComponents/SafeAreaPanel.cs
@@ -4,7 +4,7 @@ namespace Project.Scripts.UIComponents
 {
     public class SafeAreaPanel : MonoBehaviour
     {
-        private RectTransform _panel; 
+        private RectTransform _panel;
 
         void Awake()
         {
@@ -17,13 +17,12 @@ namespace Project.Scripts.UIComponents
         public static(Vector2, Vector2) GetSafeAreaAnchor()
         {
             var safeArea = Screen.safeArea;
-
             var anchorMin = safeArea.position;
             var anchorMax = safeArea.position + safeArea.size;
             anchorMin.x /= Screen.width;
             anchorMin.y /= Screen.height;
             anchorMax.x /= Screen.width;
-            anchorMax.y /= Screen.height;
+            anchorMax.y = StatusBarController.GetStatusBarBottomAnchor();
             return (anchorMin, anchorMax);
         }
     }
--- Assets/Project/Scripts/UIComponents/StatusBarController.cs
+++ Assets/Project/Scripts/UIComponents/StatusBarController.cs
@@ -1,9 +1,14 @@
-﻿namespace Project.Scripts.UIComponents
+﻿using UnityEngine;
+#if UNITY_IOS
+using UnityEngine.iOS;
+#endif
+
+namespace Project.Scripts.UIComponents
 {
     /// <summary>
     /// ステータスバーの表示を行う
     /// </summary>
-    class StatusBarController
+    public static class StatusBarController
     {
         private static int _FLAG_FULLSCREEN = 0x00000400;
         private static int _FLAG_FORCE_NOT_FULLSCREEN = 0x00000800;
@@ -35,6 +40,43 @@
                     }
                 }
             }
+            #endif
+        }
+
+        /// <summary>
+        /// ステータスバーの下側のアンカーを取得する
+        /// </summary>
+        /// <returns> ステータスバーの高さを取得する</returns>
+        public static float GetStatusBarBottomAnchor()
+        {
+            #if UNITY_IOS || UNITY_EDITOR
+            var safeArea = Screen.safeArea;
+            if (Screen.height == Screen.safeArea.height)
+            {
+                // iPhone X以前の機種
+                var deviceGeneration = UnityEngine.iOS.Device.generation;
+                if (deviceGeneration == UnityEngine.iOS.DeviceGeneration.iPhone6Plus || deviceGeneration == UnityEngine.iOS.DeviceGeneration.iPhone6SPlus || deviceGeneration == UnityEngine.iOS.DeviceGeneration.iPhone7Plus || deviceGeneration == UnityEngine.iOS.DeviceGeneration.iPhone8Plus)
+                {
+                    // 高さは54px
+                    return 1f - (54f / Screen.height);
+                } else {
+                    // 高さは40px
+                    return 1f - (40f / Screen.height);
+                }
+            }
+            else
+            {
+                // SafeAreaの上側のアンカーの位置を返す
+                var anchorMax = safeArea.position + safeArea.size;
+                return anchorMax.y / Screen.height;
+            }
+            #elif UNITY_ANDROID
+            // ステータスバーの高さを取得する
+            var rectangle = new Rect();
+            var window = getWindow();
+            window.getDecorView().getWindowVisibleDisplayFrame(rectangle);
+            var statusBarHeight = rectangle.top;
+            return 1f - statusBarHeight / Screen.height;
             #endif
         }
     }
