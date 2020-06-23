b8145da98120ae7ff97f062b326f10363b3e223c
xiii111 <aoi.t19950517@gmail.com>
2020-06-23 22:14:51 +0900

    Move interfaces to a place where they're used

--- Assets/Project/Scripts/GamePlayScene/Bottle/IBottleEnterTileHandler.cs
+++ Assets/Project/Scripts/GamePlayScene/Bottle/IBottleEnterTileHandler.cs
@@ -7,31 +7,4 @@ namespace Project.Scripts.GamePlayScene.Bottle
     {
         void OnEnterTile(GameObject tile);
     }
-
-    internal class NormalBottleEnterTileHandler : IBottleEnterTileHandler
-    {
-        private readonly AbstractBottleController _bottle;
-        private readonly IBottleSuccessHandler _successHandler;
-
-        internal NormalBottleEnterTileHandler(AbstractBottleController bottle, IBottleSuccessHandler successHandler)
-        {
-            if (successHandler == null)
-                throw new System.NullReferenceException("SuccessHandler can not be null");
-
-            _bottle = bottle;
-            _successHandler = successHandler;
-        }
-
-        public void OnEnterTile(GameObject tile)
-        {
-            if (_successHandler.IsSuccess()) {
-                // 最終タイルにいるかどうかで，光らせるかを決める
-                _bottle.GetComponent<SpriteGlowEffect>().enabled = true;
-
-                _successHandler.DoWhenSuccess();
-            } else {
-                _bottle.GetComponent<SpriteGlowEffect>().enabled = false;
-            }
-        }
-    }
 }
--- Assets/Project/Scripts/GamePlayScene/Bottle/NormalBottleController.cs
+++ Assets/Project/Scripts/GamePlayScene/Bottle/NormalBottleController.cs
@@ -42,4 +42,62 @@ namespace Project.Scripts.GamePlayScene.Bottle
             finalTile.GetComponent<NormalTileController>().SetSprite(targetTileSprite);
         }
     }
+
+    internal class NormalBottleEnterTileHandler : IBottleEnterTileHandler
+    {
+        private readonly AbstractBottleController _bottle;
+        private readonly IBottleSuccessHandler _successHandler;
+
+        internal NormalBottleEnterTileHandler(AbstractBottleController bottle, IBottleSuccessHandler successHandler)
+        {
+            if (successHandler == null)
+                throw new System.NullReferenceException("SuccessHandler can not be null");
+
+            _bottle = bottle;
+            _successHandler = successHandler;
+        }
+
+        public void OnEnterTile(GameObject tile)
+        {
+            if (_successHandler.IsSuccess()) {
+                // 最終タイルにいるかどうかで，光らせるかを決める
+                _bottle.GetComponent<SpriteGlowEffect>().enabled = true;
+
+                _successHandler.DoWhenSuccess();
+            } else {
+                _bottle.GetComponent<SpriteGlowEffect>().enabled = false;
+            }
+        }
+    }
+
+    internal class NormalBottleSuccessHandler : IBottleSuccessHandler
+    {
+        /// <summary>
+        /// 目標位置
+        /// </summary>
+        private readonly int _targetPos;
+
+        /// <summary>
+        /// ボトルのインスタンス
+        /// </summary>
+        private readonly AbstractBottleController _bottle;
+
+        internal NormalBottleSuccessHandler(AbstractBottleController bottle, int targetPos)
+        {
+            _bottle = bottle;
+            _targetPos = targetPos;
+        }
+
+        public void DoWhenSuccess()
+        {
+            // ステージの成功判定
+            GameObject.FindObjectOfType<GamePlayDirector>().CheckClear();
+        }
+
+        public bool IsSuccess()
+        {
+            var currPos = BoardManager.GetBottlePos(_bottle);
+            return currPos == _targetPos;
+        }
+    }
 }
