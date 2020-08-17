634c23ff69cae364b9965db3d0294e3c420b0da5
takesi0627 <woody70825@gmail.com>
2020-08-17 22:39:33 +0900

    calculate initial position

--- Assets/Project/Scripts/GamePlayScene/Gimmick/SolarBeamController.cs
+++ Assets/Project/Scripts/GamePlayScene/Gimmick/SolarBeamController.cs
@@ -7,44 +7,73 @@ namespace Project.Scripts.GamePlayScene.Gimmick
 {
     public class SolarBeamController : AbstractGimmickController
     {
+        [SerializeField] private GameObject _sunObject;
+
+        [SerializeField] private GameObject _beamObject;
+
+        /// <summary>
+        /// 登場から警告出すまでの時間
+        /// </summary>
+        [SerializeField] private float _idleTime = 2.0f;
+
+        private const string _WARNING_TRIGGER_NAME = "Warning";
+
         private Animator _animator;
+
+        void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
 
         public override void Initialize(GimmickData gimmickData)
         {
             base.Initialize(gimmickData);
 
             var direction = gimmickData.solarBeamDirection;
-            switch (direction) {
-                case ESolarBeamDirection.Horizontal:
+            switch (direction)
+            {
+                case ESolarBeamDirection.Horizontal: {
                     var row = gimmickData.targetRow;
 
                     // 親オブジェクトの位置設定
                     var initialPos = BoardManager.Instance.GetTilePos(row, EColumn.Center);
                     transform.position = initialPos;
 
-                    // 
+                    // 太陽とビームの位置を調整する
+                    var sunRenderer = _sunObject.GetComponent<SpriteRenderer>();
+                    // 中央から1.5タイルサイズ＋1.5太陽の幅分ずらす
+                    var offset = new Vector2(TileSize.WIDTH * 1.5f + sunRenderer.size.x * 1.5f, 0);
+                    _sunObject.transform.position = initialPos + offset;
+                    sunRenderer.enabled = true;
+
                     break;
-                case ESolarBeamDirection.Vertical:
+                }
+                case ESolarBeamDirection.Vertical: {
+                    var col = gimmickData.targetColumn;
+
+                    // 親オブジェクトの位置設定
+                    var initialPos = BoardManager.Instance.GetTilePos(ERow.Third, col);
+                    transform.position = initialPos;
+
+                    // 太陽とビームの位置を調整する
+                    var sunRenderer = _sunObject.GetComponent<SpriteRenderer>();
+                    var offset = new Vector2(0, TileSize.HEIGHT * 2.5f + sunRenderer.size.y * 1.5f);
+                    _sunObject.transform.position = initialPos + offset;
+                    sunRenderer.enabled = true;
+
+                    _beamObject.transform.Rotate(Quaternion.Euler(0, 0, 90).eulerAngles);
+                    var beamSunDistance = Vector3.Distance(_beamObject.transform.position, _sunObject.transform.position);
+                    _beamObject.transform.position = _sunObject.transform.position + Vector3.down * beamSunDistance;
                     break;
+                }
             }
         }
 
         public override IEnumerator Trigger()
         {
-            throw new System.NotImplementedException();
-        }
+            yield return new WaitForSeconds(_idleTime);
 
-        void Start()
-        {
-            _animator = GetComponent<Animator>();
-        }
-
-        void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                _animator.SetTrigger("Warning");
-            }
+            _animator.SetTrigger(_WARNING_TRIGGER_NAME);
         }
     }
 }
