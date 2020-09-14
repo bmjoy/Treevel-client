082a94cd7b25ff82f742b39b774b219985e8c177
takesi0627 <woody70825@gmail.com>
2020-09-15 00:02:59 +0900

    fix stage data editro

--- Assets/Project/Scripts/Editor/StageDataEditor.cs
+++ Assets/Project/Scripts/Editor/StageDataEditor.cs
@@ -402,7 +402,27 @@ namespace Project.Scripts.Editor
                             });
                             break;
                         }
-                    case EGimmickType.GustWind:
+                    case EGimmickType.GustWind:{
+                            // 攻撃方向
+                            var directionProp = gimmickDataProp.FindPropertyRelative("targetDirection");
+                            EditorGUILayout.PropertyField(directionProp);
+                            switch ((EGimmickDirection)directionProp.intValue) {
+                                case EGimmickDirection.ToRight:
+                                case EGimmickDirection.ToLeft:
+                                    EditorGUILayout.PropertyField(gimmickDataProp.FindPropertyRelative("targetRow"));
+                                    break;
+                                case EGimmickDirection.ToUp:
+                                case EGimmickDirection.ToBottom:
+                                    EditorGUILayout.PropertyField(gimmickDataProp.FindPropertyRelative("targetColumn"));
+                                    break;
+                                default:
+                                    // 描画を止めないように適当な値を設定する
+                                    Debug.LogWarning($"Invalid Enum Value: {directionProp.intValue}");
+                                    directionProp.intValue = (int)EGimmickDirection.ToLeft;
+                                    break;
+                            }
+                            break;
+                        }
                     case EGimmickType.SolarBeam: {
                             // 攻撃回数
                             var attackTimesProp = gimmickDataProp.FindPropertyRelative("attackTimes");
