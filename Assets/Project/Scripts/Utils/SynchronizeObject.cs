8605ff9ae6ae1386bac909c435458eaba17acb7b
takasusuke <gendaijin44435@gmail.com>
2020-09-16 00:38:16 +0900

    Synchronize two canvas position and scale in LevelSelectScene

---
+++ Assets/Project/Scripts/Utils/SynchronizeObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project.Scripts.Utils
+{
+    public class SynchronizeObject : MonoBehaviour
+    {
+        [SerializeField] private GameObject synchronizedObject;
+
+        /// <summary>
+        /// 対象のGameObjectの位置・大きさを主とするGameObjectと同じにする
+        /// </summary>
+        void Update()
+        {
+            synchronizedObject.transform.localPosition = gameObject.transform.localPosition;
+            synchronizedObject.transform.localScale = gameObject.transform.localScale;
+        }
+    }
+}
