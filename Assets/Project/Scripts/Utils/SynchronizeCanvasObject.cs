4e2bc522349b7af349f353becdb63336c0455fe3
takasusuke <gendaijin44435@gmail.com>
2020-09-18 13:57:23 +0900

    Reflect content when scroll

---
+++ Assets/Project/Scripts/Utils/SynchronizeCanvasObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Project.Scripts.Utils
+{
+    [DefaultExecutionOrder(1)]
+    public class SynchronizeCanvasObject : MonoBehaviour
+    {
+        [SerializeField] private ScrollRect _scrollRect;
+
+        /// <summary>
+        /// 位置・拡大縮小率を反映させるオブジェクト
+        /// </summary>
+        /// <returns></returns>
+        [SerializeField] private GameObject _synchronizedObject;
+
+        void Awake() {
+            _scrollRect.onValueChanged.AddListener(delegate {
+                _synchronizedObject.transform.localPosition = gameObject.transform.localPosition;
+                _synchronizedObject.transform.localScale = gameObject.transform.localScale;
+            });
+        }
+
+        void OnEnable() {
+            _synchronizedObject.transform.localPosition = gameObject.transform.localPosition;
+            _synchronizedObject.transform.localScale = gameObject.transform.localScale;
+        }
+    }
+}
