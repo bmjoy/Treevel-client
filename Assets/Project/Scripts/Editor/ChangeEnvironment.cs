091316d1313dcad0d5a637bbb6195cdb76590530
xiii111 <aoi.t19950517@gmail.com>
2020-10-04 09:34:31 +0900

    Change environment sysmbol from editor

---
+++ Assets/Project/Scripts/Editor/ChangeEnvironment.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+
+namespace Project.Scripts.Editor
+{
+    public static class ChangeEnvironment
+    {
+        // 開発環境に切り替え
+        [MenuItem("Environment/DEV")]
+        public static void Develop(){
+            var symbols = GetSymbols();
+
+            if (symbols.Contains("ENV_DEV")) return;
+
+            symbols.Remove("ENV_PROD");
+            Menu.SetChecked("Environment/PROD", false);
+
+            symbols.Add("ENV_DEV");
+            Menu.SetChecked("Environment/DEV", true);
+
+            SetSymbols(symbols);
+        }
+
+        // 本番環境に切り替え
+        [MenuItem("Environment/PROD")]
+        public static void Release(){
+            var symbols = GetSymbols();
+
+            if (symbols.Contains("ENV_PROD")) return;
+
+            symbols.Remove("ENV_DEV");
+            Menu.SetChecked("Environment/DEV", false);
+
+            symbols.Add("ENV_PROD");
+            Menu.SetChecked("Environment/PROD", true);
+
+            SetSymbols(symbols);
+        }
+
+        // 設定されているシンボル定義を取得する
+        private static List<string> GetSymbols(){
+            return PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup).Split(';').ToList();
+        }
+
+        // シンボル定義をセットする
+        private static void SetSymbols(List<string> symbols){
+            var symbolStr = string.Empty;
+            symbols.ForEach(s => symbolStr += s + ";");
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, symbolStr);
+        }
+    }
+}
