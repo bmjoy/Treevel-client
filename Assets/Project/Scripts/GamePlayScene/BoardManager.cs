fe3732d51a7d46942d00b3a60ebd255a43d1334e
takasusuke <gendaijin44435@gmail.com>
2020-10-09 00:17:31 +0900

    Handle when not exists bottle in dictionary

--- Assets/Project/Scripts/GamePlayScene/BoardManager.cs
+++ Assets/Project/Scripts/GamePlayScene/BoardManager.cs
@@ -347,7 +347,7 @@ namespace Project.Scripts.GamePlayScene
         /// <returns> タイル番号 </returns>
         public int GetBottlePos(AbstractBottleController bottle)
         {
-            // ボトルは必ず盤面内に収まっているので，強制アンラップ
+            if (!_bottlePositions.ContainsKey(bottle.gameObject)) return -1;
             return XYToTileNum(_bottlePositions[bottle.gameObject]).Value;
         }
 
