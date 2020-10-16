5a01ed35951dd3be96243431c89eb6b7ec2998bd
takasusuke <gendaijin44435@gmail.com>
2020-10-16 21:39:46 +0900

    Add files to implement (just created files)

---
+++ Assets/Project/Scripts/Modules/GamePlayScene/Bottle/DarkEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Treevel.Modules.GamePlayScene.Bottle
+{
+    [RequireComponent(typeof(Animator))]
+    public class DarkEffect : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
