3d21584faa47fda12a7af1229073d86afb9ef611
takasusuke <gendaijin44435@gmail.com>
2020-10-26 22:16:31 +0900

    Prepare FogPrefab

---
+++ Assets/Project/Scripts/Modules/GamePlayScene/Gimmick/FogController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Treevel.Modules.GamePlayScene.Gimmick
+{
+    public class FogController : AbstractGimmickController
+    {
+        public override IEnumerator Trigger()
+        {
+            yield return null;
+        } 
+
+        protected override void OnEndGame()
+        {
+        }
+    }
+}
