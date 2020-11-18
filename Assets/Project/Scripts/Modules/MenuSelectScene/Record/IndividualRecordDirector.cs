7d3f034ccc4496f19617be05b6336af571c64a90
xiii111 <aoi.t19950517@gmail.com>
2020-11-19 00:19:19 +0900

    Transition to `StageSelectScene`

--- Assets/Project/Scripts/Modules/MenuSelectScene/Record/IndividualRecordDirector.cs
+++ Assets/Project/Scripts/Modules/MenuSelectScene/Record/IndividualRecordDirector.cs
@@ -1,8 +1,29 @@
-﻿using UnityEngine;
+﻿using Treevel.Common.Entities;
+using UnityEngine;
+using UnityEngine.UI;
+using Treevel.Common.Managers;
+using Treevel.Common.Utils;
+using Treevel.Modules.StageSelectScene;
 
 namespace Treevel.Modules.MenuSelectScene.Record
 {
     public class IndividualRecordDirector : MonoBehaviour
     {
+        /// <summary>
+        /// [UI] "ステージ一覧へ"
+        /// </summary>
+        [SerializeField] private Button _toStageSelectSceneButton;
+
+        private void Awake()
+        {
+
+            _toStageSelectSceneButton.onClick.AddListener(() =>
+            {
+                // TODO: 季節，木を拡張したら，こちらも拡張する
+                StageSelectDirector.levelName = ELevelName.Easy;
+                StageSelectDirector.treeId = ETreeId.Spring_1;
+                AddressableAssetManager.LoadScene(Constants.SceneName.SPRING_STAGE_SELECT_SCENE);
+            });
+        }
     }
 }
