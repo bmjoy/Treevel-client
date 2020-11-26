a05886885f15aea50ef56ba65c26067e7474312f
takesi0627 <woody70825@gmail.com>
2020-11-27 01:29:44 +0900

    implement script

---
+++ Assets/Project/Scripts/Common/Components/UIs/MessageDialog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Treevel.Common.Entities;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Treevel.Common.Components.UIs
+{
+    public class MessageDialog : MonoBehaviour
+    {
+        [SerializeField] private MultiLanguageText _message;
+        [SerializeField] private Button _okButton;
+        [SerializeField] private MultiLanguageText _okButtonText;
+        [SerializeField] private Button _cancelButton;
+        [SerializeField] private MultiLanguageText _cancelButtonText;
+
+
+        public void Initialize(ETextIndex message, ETextIndex okText, Action okCallBack)
+        {
+            // メッセージ設定
+            _message.TextIndex = message;
+
+            // OKボタンの文字
+            _okButtonText.TextIndex = okText;
+
+            // コールバック設定
+            _okButton.onClick.RemoveAllListeners();
+            _okButton.onClick.AddListener(() => okCallBack.Invoke());
+        }
+
+    }
+}
