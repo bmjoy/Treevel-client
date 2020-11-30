960866a7c15e5cbf053ff756f46774a160f6b75f
xiii111 <aoi.t19950517@gmail.com>
2020-11-30 09:52:36 +0900

    Add component to button in prefabs

--- Assets/Project/Scripts/Editor/ButtonClickBlockerAdder.cs
+++ Assets/Project/Scripts/Editor/ButtonClickBlockerAdder.cs
@@ -13,6 +13,18 @@ namespace Treevel.Editor
         [MenuItem("Tools/Add Button Click Blocker")]
         private static void AddButtonClickBlocker()
         {
+            // Prefab への変更
+            var prefabGuids = AssetDatabase.FindAssets("t:Prefab", new[] {"Assets/Project"});
+
+            foreach (var prefabGuid in prefabGuids) {
+                var path = AssetDatabase.GUIDToAssetPath(prefabGuid);
+                var contentsRoot = PrefabUtility.LoadPrefabContents(path);
+
+                if (ChangeButtons()) { PrefabUtility.SaveAsPrefabAsset(contentsRoot, path); }
+                PrefabUtility.UnloadPrefabContents(contentsRoot);
+            }
+
+            // Scene への変更
             var currentScene = SceneManager.GetActiveScene().path;
             var sceneGuids = AssetDatabase.FindAssets("t:Scene", new[] { "Assets/Project" });
 
@@ -20,20 +32,32 @@ namespace Treevel.Editor
                 var path = AssetDatabase.GUIDToAssetPath(sceneGuid);
                 EditorSceneManager.OpenScene(path);
 
-                Resources.FindObjectsOfTypeAll(typeof(Button))
-                    .Select(button => button as Button)
-                    .Where(button => button != null)
-                    .Where(button => button.gameObject.GetComponent<ButtonClickBlocker>() == null)
-                    .Where(button => button.hideFlags != HideFlags.HideAndDontSave)
-                    .ToList()
-                    .ForEach(button => {
-                        button.gameObject.AddComponent<ButtonClickBlocker>();
-                    });
-
-                EditorSceneManager.SaveScene(SceneManager.GetSceneByPath(path));
+                if (ChangeButtons()) { EditorSceneManager.SaveScene(SceneManager.GetSceneByPath(path)); }
             }
 
             if (!string.IsNullOrEmpty(currentScene)) EditorSceneManager.OpenScene(currentScene);
+        }
+
+        /// <summary>
+        /// Button に変更を加える
+        /// </summary>
+        /// <returns> 変更されたかどうか </returns>
+        private static bool ChangeButtons()
+        {
+            var hasChanged = false;
+
+            Resources.FindObjectsOfTypeAll(typeof(Button))
+                .Select(button => button as Button)
+                .Where(button => button != null)
+                .Where(button => button.gameObject.GetComponent<ButtonClickBlocker>() == null)
+                .Where(button => button.hideFlags != HideFlags.HideAndDontSave)
+                .ToList()
+                .ForEach(button => {
+                    button.gameObject.AddComponent<ButtonClickBlocker>();
+                    hasChanged = true;
+                });
+
+            return hasChanged;
         }
     }
 }
