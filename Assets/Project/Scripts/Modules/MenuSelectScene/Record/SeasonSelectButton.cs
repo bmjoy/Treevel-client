a5febed9ca4bd53e807c97eb0d7603e27a8c1211
takeshi <woody70825@gmail.com>
2020-12-17 02:22:33 +0900

    Season button color transition

---
+++ Assets/Project/Scripts/Modules/MenuSelectScene/Record/SeasonSelectButton.cs
@@ -0,0 +1,27 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Treevel.Modules.MenuSelectScene.Record
+{
+    public class SeasonSelectButton : MonoBehaviour
+    {
+        [SerializeField] private Color _selectedColor;
+
+        private Toggle _toggle;
+
+        private void Awake()
+        {
+            _toggle = GetComponent<Toggle>();
+            _toggle.OnValueChangedAsObservable().Subscribe(selected =>
+            {
+                var image = _toggle.targetGraphic.GetComponent<Image>();
+                if (selected)
+                    image.color = _selectedColor;
+                else
+                    image.color = Color.clear;
+            }).AddTo(this);
+        }
+    }
+}
+
