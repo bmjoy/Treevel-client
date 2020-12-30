486b4c34b105d62a82d05b6bf243b56e374358c7
takeshi <woody70825@gmail.com>
2020-12-31 00:22:27 +0900

    refactor: ResetController.cs

--- Assets/Project/Scripts/Modules/MenuSelectScene/Settings/ResetController.cs
+++ Assets/Project/Scripts/Modules/MenuSelectScene/Settings/ResetController.cs
@@ -3,6 +3,7 @@ using Treevel.Common.Managers;
 using Treevel.Common.Utils;
 using Treevel.Modules.MenuSelectScene.LevelSelect;
 using Treevel.Modules.StageSelectScene;
+using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -18,20 +19,13 @@ namespace Treevel.Modules.MenuSelectScene.Settings
         private void Awake()
         {
             _resetButton = GetComponent<Button>();
-            _resetButton.onClick.AddListener(ResetButtonDown);
-        }
-
-        /// <summary>
-        /// ステージリセットボタンを押した場合の処理
-        /// </summary>
-        private static void ResetButtonDown()
-        {
-            // 確認用のメッセージダイアログを表示
-            UIManager.Instance.CreateOkCancelMessageDialog(
-                ETextIndex.RecordResetConfirmDialogMessage,
-                ETextIndex.MessageDlgOkBtnText,
-                ResetData
-            );
+            _resetButton.onClick.AsObservable().Subscribe(_ => {
+                UIManager.Instance.CreateOkCancelMessageDialog(
+                    ETextIndex.RecordResetConfirmDialogMessage,
+                    ETextIndex.MessageDlgOkBtnText,
+                    ResetData
+                );
+            }).AddTo(this);
         }
 
         private static void ResetData()
