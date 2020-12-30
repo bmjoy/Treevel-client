d3402d97d8499b8d848b3b4d7f4f50afc43d34bc
takeshi <woody70825@gmail.com>
2020-12-30 18:37:19 +0900

    refactor: remove to default clicked event

--- Assets/Project/Scripts/Modules/MenuSelectScene/Settings/ToDefaultController.cs
+++ Assets/Project/Scripts/Modules/MenuSelectScene/Settings/ToDefaultController.cs
@@ -7,14 +7,6 @@ namespace Treevel.Modules.MenuSelectScene.Settings
 {
     public class ToDefaultController : MonoBehaviour
     {
-        public delegate void ChangeAction();
-
-        /// <summary>
-        /// デフォルトボタンが押された際のイベント
-        /// </summary>
-        private static readonly Subject<Unit> _onToDefaultClickedSubject = new Subject<Unit>();
-        public static readonly IObservable<Unit> OnToDefaultClicked = _onToDefaultClickedSubject.AsObservable();
-
         /// <summary>
         /// デフォルトに戻すボタンを押した場合の処理
         /// </summary>
@@ -22,9 +14,6 @@ namespace Treevel.Modules.MenuSelectScene.Settings
         {
             // 設定の更新
             UserSettings.ToDefault();
-
-            // Canvasの更新
-            _onToDefaultClickedSubject.OnNext(Unit.Default);
         }
     }
 }
