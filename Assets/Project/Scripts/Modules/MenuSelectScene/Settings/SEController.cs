e51d02bfdf7985326f7aafc0c43b7fc99c56d357
Takeshi <woody70825@gmail.com>
2021-01-11 13:07:24 +0900

    fix comment
    Co-authored-by: takasu <aimaru.kei.lock@ezweb.ne.jp>

--- Assets/Project/Scripts/Modules/MenuSelectScene/Settings/SEController.cs
+++ Assets/Project/Scripts/Modules/MenuSelectScene/Settings/SEController.cs
@@ -14,7 +14,7 @@ namespace Treevel.Modules.MenuSelectScene.Settings
             // ユーザ設定が別のところで変えられた場合スライダーに反映
             UserSettings.BGMVolume.Subscribe(volume => slider.value = volume).AddTo(this);
 
-            // BGMスライダーが変化した場合の処理
+            // SEスライダーが変化した場合の処理
             slider.onValueChanged.AsObservable()
                 .Where(v => 0.0f <= v && v <= 1.0f)
                 .Subscribe(v => UserSettings.SEVolume.Value = v)
