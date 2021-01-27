bef27ade88ad212bf9022c92a65f17eb9bec2fac
xiii111 <aoi.t19950517@gmail.com>
2021-01-27 20:06:35 +0900

    Implement erasable animation

--- Assets/Project/Scripts/Modules/GamePlayScene/Bottle/ErasableBottleController.cs
+++ Assets/Project/Scripts/Modules/GamePlayScene/Bottle/ErasableBottleController.cs
@@ -1,17 +1,34 @@
 ﻿using TouchScript.Gestures;
 using Treevel.Common.Utils;
 using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 
 namespace Treevel.Modules.GamePlayScene.Bottle
 {
     [RequireComponent(typeof(TapGesture))]
+    [RequireComponent(typeof(Animator))]
     public class ErasableBottleController : StaticBottleController
     {
         /// <summary>
         /// タップジェスチャー
         /// </summary>
         private TapGesture _tapGesture;
+
+        /// <summary>
+        /// アニメーター
+        /// </summary>
+        private Animator _animator;
+
+        /// <summary>
+        /// Out パラメーター
+        /// </summary>
+        private static readonly int _ANIMATOR_PARAM_OUT = Animator.StringToHash("Out");
+
+        /// <summary>
+        /// Out ステート
+        /// </summary>
+        private static readonly int _ANIMATOR_STATE_OUT = Animator.StringToHash("Erasable@out");
 
         protected override void Awake()
         {
@@ -21,11 +38,23 @@ namespace Treevel.Modules.GamePlayScene.Bottle
             name = Constants.BottleName.ERASABLE_BOTTLE;
             #endif
 
+            _animator = GetComponent<Animator>();
+
             // TapGesture の設定
             _tapGesture = GetComponent<TapGesture>();
             _tapGesture.UseUnityEvents = true;
             _tapGesture.OnTap.AsObservable()
                 .Subscribe(_ => {
+                    // 退出させる
+                    _animator.SetBool(_ANIMATOR_PARAM_OUT, true);
+                })
+                .AddTo(this);
+
+            _animator.GetBehaviour<ObservableStateMachineTrigger>()
+                .OnStateExitAsObservable()
+                .Where(state => state.StateInfo.shortNameHash == _ANIMATOR_STATE_OUT)
+                .Subscribe(_ => {
+                    // 退出が終わった後の処理
                     Destroy(gameObject);
                 })
                 .AddTo(this);
