31b89cbbfdcd0c221a60e1e92b36097c99134fd2
takeshi <woody70825@gmail.com>
2021-02-07 16:36:23 +0900

    fix namespace usage
    it failed when iOS or Android build without #if UNITY_EDITOR because the namespace UnityEditor doesn't exist in the environment out of the Unity Editor

--- Assets/Project/Scripts/Common/Managers/SoundManager.cs
+++ Assets/Project/Scripts/Common/Managers/SoundManager.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using Treevel.Common.Entities;
 using Treevel.Common.Patterns.Singleton;
-using UnityEditor;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Treevel.Common.Managers
 {
