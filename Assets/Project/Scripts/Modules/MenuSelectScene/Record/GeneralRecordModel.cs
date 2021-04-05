3ec4e67a6a354b07ab4cdcbd4570ebe7ddf24d3f
xiii111 <aoi.t19950517@gmail.com>
2021-04-05 22:04:21 +0900

    Create model

---
+++ Assets/Project/Scripts/Modules/MenuSelectScene/Record/GeneralRecordModel.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using Treevel.Common.Entities;
+using Treevel.Common.Managers;
+using Treevel.Common.Networks;
+using Treevel.Common.Networks.Requests;
+using UniRx;
+
+namespace Treevel.Modules.MenuSelectScene.Record
+{
+    public class GeneralRecordModel
+    {
+        private readonly CompositeDisposable _disposable = new CompositeDisposable();
+
+        /// <summary>
+        /// ステージの情報
+        /// </summary>
+        public readonly ReactiveProperty<StageStatus[]> stageStatusArray = new ReactiveProperty<StageStatus[]>();
+
+        /// <summary>
+        /// 起動日数
+        /// </summary>
+        public readonly ReactiveProperty<int> startupDays = new ReactiveProperty<int>();
+
+        /// <summary>
+        /// 各ギミックの失敗回数
+        /// </summary>
+        public readonly ReactiveProperty<Dictionary<EFailureReasonType, int>> failureReasonCount = new ReactiveProperty<Dictionary<EFailureReasonType, int>>();
+
+        public GeneralRecordModel()
+        {
+            FetchStageStatusArrayAsync().Forget();
+
+            RecordData.Instance.StartupDaysObservable
+                .Subscribe(startupDays => this.startupDays.Value = startupDays)
+                .AddTo(_disposable);
+
+            RecordData.Instance.failureReasonCount
+                .Subscribe(failureReasonCount => this.failureReasonCount.Value = failureReasonCount)
+                .AddTo(_disposable);
+        }
+
+        public async UniTask FetchStageStatusArrayAsync()
+        {
+            var tasks = GameDataManager.GetAllStages()
+                .Select(stage => NetworkService.Execute(new GetStageStatusRequest(stage.TreeId, stage.StageNumber)));
+
+            stageStatusArray.Value = await UniTask.WhenAll(tasks);
+        }
+
+        public void Dispose()
+        {
+            _disposable.Dispose();
+        }
+    }
+}
