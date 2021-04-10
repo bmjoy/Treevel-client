0a1e9ff34509188a1c3e3624ef27da0616d61c80
takeshi <woody70825@gmail.com>
2021-04-11 00:21:36 +0900

    branch release logic

--- Assets/Project/Scripts/Modules/StageSelectScene/BranchController.cs
+++ Assets/Project/Scripts/Modules/StageSelectScene/BranchController.cs
@@ -15,15 +15,7 @@ namespace Treevel.Modules.StageSelectScene
         /// </summary>
         [SerializeField] private ETreeId _treeId;
 
-        private StageController _endObjectController;
-
-        public static Dictionary<string, bool> branchStates;
-
-        protected override void Awake()
-        {
-            base.Awake();
-            _endObjectController = endObject.GetComponent<StageController>();
-        }
+        public static List<string> animationPlayedBranches;
 
         protected override void SetSaveKey()
         {
@@ -36,30 +28,16 @@ namespace Treevel.Modules.StageSelectScene
         /// </summary>
         public override UniTask UpdateStateAsync()
         {
-            if (branchStates.ContainsKey(saveKey)) {
-                released = branchStates[saveKey];
+            if (constraintObjects.Length == 0) return UniTask.CompletedTask;
+
+            if (constraintObjects.Select(go => go.GetComponent<StageController>()).All(x => x.state == EStageState.Cleared)) {
+                if (!animationPlayedBranches.Contains(saveKey)) {
+                    // TODO 枝解放時演出
+                    Debug.Log($"{saveKey}が解放された");
+                    animationPlayedBranches.Add(saveKey);
+                }
             } else {
-                released = false;
-            }
-
-            if (!released) {
-                if (constraintObjects.Length == 0) {
-                    // 初期状態で解放されている枝
-                    released = true;
-                } else {
-                    released = constraintObjects.All(stage => stage.GetComponent<StageController>().state >=
-                                                              EStageState.Cleared);
-                }
-
-                if (released) {
-                    // 終点のステージの状態の更新
-                    _endObjectController.ReleaseStage();
-                    _endObjectController.ReflectTreeState();
-                }
-            }
-
-            if (!released) {
-                // 非解放時
+                // 未解放
                 lineRenderer.startColor = new Color(0.2f, 0.2f, 0.7f);
                 lineRenderer.endColor = new Color(0.2f, 0.2f, 0.7f);
             }
@@ -72,7 +50,7 @@ namespace Treevel.Modules.StageSelectScene
         /// </summary>
         public override void SaveState()
         {
-            branchStates[saveKey] = released;
+            // branchStates[saveKey] = released;
         }
     }
 }
