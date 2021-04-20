09b9095d2cb3d3edb6bb4cf54d257d6a37ba9ff9
xiii111 <aoi.t19950517@gmail.com>
2021-04-20 21:18:13 +0900

    Leave unused constructor

--- Assets/Project/Scripts/Common/Networks/Requests/UpdateStageStatusRequest.cs
+++ Assets/Project/Scripts/Common/Networks/Requests/UpdateStageStatusRequest.cs
@@ -5,6 +5,12 @@ namespace Treevel.Common.Networks.Requests
 {
     public class UpdateStageStatusRequest : UpdateServerRequestBase<StageStatus>
     {
+        public UpdateStageStatusRequest(ETreeId treeId, int stageNumber, StageStatus data)
+        {
+            key = StageData.EncodeStageIdKey(treeId, stageNumber);
+            this.data = data;
+        }
+
         public UpdateStageStatusRequest(string key, StageStatus data)
         {
             this.key = key;
