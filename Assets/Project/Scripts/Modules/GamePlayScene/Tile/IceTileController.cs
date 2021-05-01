042ca47e4cdd4401b5a6c810944972b6cd57212f
takasusuke <gendaijin44435@gmail.com>
2021-05-02 00:41:31 +0900

    Add comment (https://github.com/LITO-apps/Treevel-client/pull/898#discussion_r621120139)

--- Assets/Project/Scripts/Modules/GamePlayScene/Tile/IceTileController.cs
+++ Assets/Project/Scripts/Modules/GamePlayScene/Tile/IceTileController.cs
@@ -23,6 +23,7 @@ namespace Treevel.Modules.GamePlayScene.Tile
             }).AddTo(compositeDisposableOnGameEnd, this);
             GamePlayDirector.Instance.GameStart.Subscribe(_ => _iceLayer.material = AddressableAssetManager.GetAsset<Material>(Constants.Address.ICE_LAYER_MATERIAL)).AddTo(compositeDisposableOnGameEnd, this);
             GamePlayDirector.Instance.GameEnd.Subscribe(_ => {
+                // Spriteのデフォルトのマテリアルを取得(実機、エディタ上両対応)
                 #if UNITY_EDITOR
                 _iceLayer.material = UnityEditor.AssetDatabase.GetBuiltinExtraResource<Material>("Sprites-Default.mat");
                 #else
